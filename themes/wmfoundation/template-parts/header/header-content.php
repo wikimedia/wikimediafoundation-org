9dc71c6b26ec51e8187b7570eba32e264e4f1545
Nick Croft <nick@copyblogger.com>
2018-02-22 08:44:02 -0500

    wpcs fixes

--- themes/wmfoundation/template-parts/header/header-content.php
+++ themes/wmfoundation/template-parts/header/header-content.php
@@ -16,7 +16,7 @@ $title    = ! empty( $page_header_data['h1_title'] ) ? $page_header_data['h1_tit
 ?>
 
 <div class="header-content">
-	
+
 	<?php if ( ! empty( $h4_title ) ) : ?>
 	<h2 class="h4 uppercase eyebrow">
 		<?php if ( ! empty( $h4_link ) ) : ?>
@@ -40,7 +40,7 @@ $title    = ! empty( $page_header_data['h1_title'] ) ? $page_header_data['h1_tit
 		<?php endif; ?>
 		</h2>
 	<?php endif; ?>
-	
+
 	<?php if ( ! empty( $mar_bottom ) ) : ?>
 	<h1 class="mar-bottom"><?php echo esc_html( $mar_bottom ); ?></h1>
 	<?php endif; ?>
