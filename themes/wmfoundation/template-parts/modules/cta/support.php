74d0e6320d39dc84f1ce54f4da4f3d219c99daa0
Nick Croft <nick@copyblogger.com>
2018-02-27 10:50:15 -0500

    use shared template for page specific CTA and support

--- themes/wmfoundation/template-parts/modules/cta/support.php
+++ themes/wmfoundation/template-parts/modules/cta/support.php
@@ -5,42 +5,20 @@
  * @package wmfoundation
  */
 
-$image_id  = get_theme_mod( 'wmf_support_image' );
-$image     = is_numeric( $image_id ) ? wp_get_attachment_image_url( $image_id, 'image_16x9_large' ) : '';
-$heading   = get_theme_mod( 'wmf_support_heading' );
-$content   = get_theme_mod( 'wmf_support_content' );
-$link_uri  = get_theme_mod( 'wmf_support_link_uri' );
-$link_text = get_theme_mod( 'wmf_support_link_text' );
+$image_id = get_theme_mod( 'wmf_support_image' );
+$image    = is_numeric( $image_id ) ? wp_get_attachment_image_url( $image_id, 'image_16x9_large' ) : '';
 
-if ( empty( $link_uri ) || empty( $link_text ) ) {
+$template_args = array(
+	'class'     => 'cta-primary img-left-content-right bg-img--pink btn-blue',
+	'image'     => $image,
+	'heading'   => get_theme_mod( 'wmf_support_heading' ),
+	'content'   => get_theme_mod( 'wmf_support_content' ),
+	'link_uri'  => get_theme_mod( 'wmf_support_link_uri' ),
+	'link_text' => get_theme_mod( 'wmf_support_link_text' ),
+);
+
+if ( empty( $template_args['link_uri'] ) || empty( $template_args['link_text'] ) ) {
 	return; // CTAs need links.
 }
 
-?>
-
-<div class="w-100p cta mod-margin-bottom cta-primary img-left-content-right bg-img--pink btn-blue">
-	<div class="mw-1360">
-		<div class="card">
-			<?php if ( ! empty( $image ) ) : ?>
-			<div class="bg-img-container">
-				<div class="bg-img" style="background-image: url(<?php echo esc_url( $image ); ?>)"></div>
-			</div>
-			<?php endif; ?>
-
-			<div class="card-content w-45p">
-
-				<?php if ( ! empty( $heading ) ) : ?>
-				<h2 class="h2"><?php echo esc_html( $heading ); ?></h2>
-				<?php endif; ?>
-
-				<?php if ( ! empty( $content ) ) : ?>
-					<p><?php echo esc_html( $content ); ?></p>
-				<?php endif; ?>
-
-				<a class="btn " href="<?php echo esc_url( $link_uri ); ?>"><?php echo esc_html( $link_text ); ?></a>
-
-			</div>
-		</div>
-
-	</div>
-</div>
+wmf_get_template_part( 'template-parts/modules/cta/page', $template_args );
