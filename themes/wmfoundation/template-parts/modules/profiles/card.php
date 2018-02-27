91f2bf180098ba309581f3924c5ceb7cae08672a
Jay Hoffmann <jhoffm34@gmail.com>
2018-02-27 10:05:02 -0500

    Adding profile template to landing page

--- themes/wmfoundation/template-parts/modules/profiles/card.php
+++ themes/wmfoundation/template-parts/modules/profiles/card.php
@@ -7,35 +7,38 @@
 
 $template_data = wmf_get_template_data();
 
-$link = ! empty( $template_data['link'] ) ? $template_data['link'] : '';
-$title = ! empty( $template_data['title'] ) ? $template_data
+$link   = ! empty( $template_data['link'] ) ? $template_data['link'] : '';
+$title  = ! empty( $template_data['title'] ) ? $template_data
 ['title'] : '';
-$role = ! empty( $template_data['role'] ) ? $template_data
+$role   = ! empty( $template_data['role'] ) ? $template_data
 ['role'] : '';
+$team   = ! empty( $template_data['team'] ) ? $template_data
+['team'] : '';
 $img_id = ! empty( $template_data['img_id'] ) ? $template_data
 ['img_id'] : '';
 
 ?>
 
 <a class="card card-vertical card-person hover-img-zoom w-32p" href="<?php echo esc_url( $link ); ?>">
-<div class="card-content top-into-img">
 
 	<?php if ( ! empty( $img_id ) ) : ?>
 	<div class="img-container">
-		<?php echo wp_get_attachment_image( $img_id, 'img_large_4x5' ); ?>
+		<?php echo wp_get_attachment_image( $img_id, 'image_4x5_large' ); ?>
 	</div>
 	<?php endif; ?>
 
-	<?php if ( ! empty( $title ) ) : ?>
-	<h5 class="person-name">
-		<?php echo esc_html( $title ); ?>
-	</h5>
-	<?php endif; ?>
+	<div class="card-content top-into-img">
+		<?php if ( ! empty( $title ) ) : ?>
+		<h5 class="person-name">
+			<?php echo esc_html( $title ); ?>
+		</h5>
+		<?php endif; ?>
 
-	<?php if ( ! empty( $role ) ) : ?>
-		<span class="person-title p color-gray"><?php echo esc_html( $role ); ?></span>
-	<?php endif; ?>
-
-</div>
+		<?php if ( ! empty( $role ) || ! empty( $team ) ) : ?>
+			<span class="person-title p color-gray">
+				<?php printf( esc_html__( '%1$s, %2$s', 'wmfoundation' ), esc_html( $role ), esc_html( $team ) ); ?>
+			</span>
+		<?php endif; ?>
+	</div>
 
 </a>
--- themes/wmfoundation/template-parts/modules/profiles/list.php
+++ themes/wmfoundation/template-parts/modules/profiles/list.php
@@ -12,10 +12,10 @@ if ( empty( $profiles ) || ! is_array( $profiles ) || count( $profiles ) < 3 ) {
 }
 
 $profile_list = array_rand( array_flip( $profiles ), 3 );
-$headline = ! empty( $template_data['headline'] ) ? $template_data['headline'] : '';
+$headline     = ! empty( $template_data['headline'] ) ? $template_data['headline'] : '';
 
 $default_pre_heading = get_post_type_object( 'profile' )->label;
-$pre_heading = ! empty( $template_data['pre_heading'] ) ? $template_data['pre_heading'] : $default_pre_heading;
+$pre_heading         = ! empty( $template_data['pre_heading'] ) ? $template_data['pre_heading'] : $default_pre_heading;
 ?>
 
 <div class="w-100p white-bg mod-margin-bottom">
@@ -29,13 +29,22 @@ $pre_heading = ! empty( $template_data['pre_heading'] ) ? $template_data['pre_he
 		<?php endif; ?>
 
 		<div class="people slider-on-mobile flex flex-medium">
-		<?php foreach ( $profile_list as $profile_id ) {
-			wmf_get_template_part( 'template-parts/modules/profiles/card', array(
-				'title' => get_the_title( $profile_id ),
-				'img_id' => get_post_thumbnail_id( $profile_id ),
-				'link' => get_the_permalink( $profile_id ),
-				'role' => get_post_meta( $profile_id, 'profile_role', true ),
-			) );
+		<?php
+		foreach ( $profile_list as $profile_id ) {
+			$team_name = '';
+			$team      = get_the_terms( $profile_id, 'role' );
+			if ( ! empty( $team ) && ! is_wp_error( $team ) ) {
+				$team_name = $team[0]->name;
+			}
+			wmf_get_template_part(
+				'template-parts/modules/profiles/card', array(
+					'title'  => get_the_title( $profile_id ),
+					'img_id' => get_post_thumbnail_id( $profile_id ),
+					'link'   => get_the_permalink( $profile_id ),
+					'role'   => get_post_meta( $profile_id, 'profile_role', true ),
+					'team'   => $team_name,
+				)
+			);
 		}
 		?>
 		</div>
