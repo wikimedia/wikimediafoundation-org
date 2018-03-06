4651d6e8e13cf134484506f1b304f0d23bf95dcd
Jay Hoffmann <jhoffm34@gmail.com>
2018-03-06 10:03:24 -0500

    Fixing categories formatting

--- themes/wmfoundation/template-parts/post-categories.php
+++ themes/wmfoundation/template-parts/post-categories.php
@@ -7,7 +7,7 @@
 
 $post_categories = wp_get_post_categories(
 	get_the_ID(), array(
-		'fields' => 'names',
+		'fields' => 'id=>name',
 	)
 );
 
@@ -19,10 +19,8 @@ if ( empty( $post_categories ) || is_wp_error( $post_categories ) ) {
 <div class="read-more flex flex-medium">
 	<h5 class="h5 color-black">Read More</h5>
 	<ul class="link-list inline-block">
-		<?php foreach ( $post_categories as $category ) : ?>
-		<li>
-			<a href="<?php echo esc_url( get_term_link( $category, 'category' ) ); ?>"><?php echo esc_html( $category ); ?></a>
-		</li>
+		<?php foreach ( $post_categories as $cat_id => $category ) : ?>
+		<li><a href="<?php echo esc_url( get_term_link( $cat_id, 'category' ) ); ?>"><?php echo esc_html( $category ); ?></a></li>
 		<?php endforeach; ?>
 	</ul>
 </div>
