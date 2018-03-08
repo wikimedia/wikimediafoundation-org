7ba931e002e966ca93d37329534c31c5f1adc77f
Nick Croft <nick@copyblogger.com>
2018-03-08 14:45:48 -0500

    js lint fixes

--- themes/wmfoundation/assets/dist/admin/customize-control.js
+++ themes/wmfoundation/assets/dist/admin/customize-control.js
@@ -3,37 +3,37 @@
 
 		init: function() {
 
-			$(window).load(function(){
+			$( window ).load( function() {
 
-				$('textarea.wp-editor-area').each(function(){
-					var tArea = $(this),
-						id = tArea.attr('id'),
+				$( 'textarea.wp-editor-area' ).each( function() {
+					var textArea = $(this),
+						id = textArea.attr('id'),
 						editor = tinyMCE.get(id),
 						setChange,
 						content;
 
-					if(editor){
-						editor.onChange.add(function (ed, e) {
+					if ( editor ) {
+						editor.onChange.add( function ( ed ) {
 							ed.save();
 							content = editor.getContent();
-							clearTimeout(setChange);
-							setChange = setTimeout(function(){
-								tArea.val(content).trigger('change');
-							},500);
+							clearTimeout( setChange );
+							setChange = setTimeout( function() {
+								textArea.val( content ).trigger( 'change' );
+							}, 500 );
 						});
 					}
 
-					tArea.css({
+					textArea.css( {
 						visibility: 'visible'
-					}).on('keyup', function(){
-						content = tArea.val();
-						clearTimeout(setChange);
-						setChange = setTimeout(function(){
-							content.trigger('change');
-						},500);
-					});
-				});
-			});
+					} ).on( 'keyup', function() {
+						content = textArea.val();
+						clearTimeout( setChange );
+						setChange = setTimeout( function(){
+							content.trigger( 'change' );
+						}, 500 );
+					} );
+				} );
+			} );
 		}
 
 	};
