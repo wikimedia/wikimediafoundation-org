b46b368449d1201daac00f197a27260e5a72990b
Jay Hoffmann <jhoffm34@gmail.com>
2018-06-21 10:16:31 -0400

    Moving save to attachment_fields_to_save

--- themes/wmfoundation/inc/fields/media.php
+++ themes/wmfoundation/inc/fields/media.php
@@ -16,17 +16,17 @@ function wmf_add_media_custom_fields( $attachment_fields, $post ) {
 	$credit_info = get_post_meta( $post->ID, 'credit_info', true );
 
 	$attachment_fields['credit_author'] = array(
-		'value' => ! empty( $credit_info['author'] ) ? $credit_info['author'] : '',
+		'value' => ! empty( $credit_info['author'] ) ? esc_html( $credit_info['author'] ) : '',
 		'label' => __( 'Author', 'wmfoundation' ),
 	);
 
 	$attachment_fields['credit_license'] = array(
-		'value' => ! empty( $credit_info['license'] ) ? $credit_info['license'] : '',
+		'value' => ! empty( $credit_info['license'] ) ? esc_html( $credit_info['license'] ) : '',
 		'label' => __( 'License', 'wmfoundation' ),
 	);
 
 	$attachment_fields['credit_url'] = array(
-		'value' => ! empty( $credit_info['url'] ) ? $credit_info['url'] : '',
+		'value' => ! empty( $credit_info['url'] ) ? esc_url( $credit_info['url'] ) : '',
 		'label' => __( 'Credit URL', 'wmfoundation' ),
 	);
 
@@ -40,21 +40,17 @@ add_filter( 'attachment_fields_to_edit', 'wmf_add_media_custom_fields', 10, 2 );
  *
  * @param int $attachment_id Attachmend post ID.
  */
-function wmf_save_attachment_custom_fields( $attachment_id ) {
-	if ( empty( $_REQUEST['attachments'][ $attachment_id ] ) ) { // // WPCS: Input var CSRF ok.
-		return;
-	}
-
+function wmf_save_attachment_custom_fields( $post, $attachment ) {
 	$credit_info = array(
-		'author'  => ! empty( $_REQUEST['attachments'][ $attachment_id ]['credit_author'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['attachments'][ $attachment_id ]['credit_author'] ) ) : '', // WPCS: Input var CSRF ok.
+		'author'  => ! empty( $attachment['credit_author'] ) ? sanitize_text_field( wp_unslash( $attachment['credit_author'] ) ) : '', // WPCS: Input var CSRF ok.
 
-		'license' => ! empty( $_REQUEST['attachments'][ $attachment_id ]['credit_license'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['attachments'][ $attachment_id ]['credit_license'] ) ) : '', // WPCS: Input var CSRF ok.
+		'license' => ! empty( $attachment['credit_license'] ) ? sanitize_text_field( wp_unslash( $attachment['credit_license'] ) ) : '', // WPCS: Input var CSRF ok.
 
-		'url'     => ! empty( $_REQUEST['attachments'][ $attachment_id ]['credit_url'] ) ? esc_url_raw( wp_unslash( $_REQUEST['attachments'][ $attachment_id ]['credit_url'] ) ) : '', // WPCS: Input var CSRF ok.
+		'url'     => ! empty( $attachment['credit_url'] ) ? esc_url_raw( wp_unslash( $attachment['credit_url'] ) ) : '', // WPCS: Input var CSRF ok.
 	);
 
 	if ( ! empty( $credit_info ) ) {
-		update_post_meta( $attachment_id, 'credit_info', $credit_info );
+		update_post_meta( $post['ID'], 'credit_info', $credit_info );
 	}
 }
-add_action( 'edit_attachment', 'wmf_save_attachment_custom_fields' );
+add_filter( 'attachment_fields_to_save', 'wmf_save_attachment_custom_fields', 10, 2 );
