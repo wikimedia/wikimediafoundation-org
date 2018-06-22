2428ea16f7181be955325520ebc7c18b178e5fe6
Jay Hoffmann <jhoffm34@gmail.com>
2018-06-22 10:09:17 -0400

    Adding new CSS + JS changes

--- themes/wmfoundation/searchform.php
+++ themes/wmfoundation/searchform.php
@@ -9,16 +9,14 @@ $wmf_search_button      = get_theme_mod( 'wmf_search_button_copy', __( 'Search',
 $wmf_search_placeholder = get_theme_mod( 'wmf_search_placeholder_copy', __( 'Enter search terms', 'wmfoundation' ) );
 ?>
 
-<div class="search-container">
-	<button class="search-toggle">
+<div class="search-bar-container">
+	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<input type="search" placeholder="<?php echo esc_attr( $wmf_search_placeholder ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 		<?php wmf_show_icon( 'search', 'material' ); ?>
-		<span class="search-label uppercase bold"><?php echo esc_html( $wmf_search_button ); ?></span>
-	</button><div class="search-bar-container">
-		<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<input type="search" placeholder="<?php echo esc_attr( $wmf_search_placeholder ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
-			<?php wmf_show_icon( 'search', 'material' ); ?>
-			<button type="submit"><?php echo esc_html( $wmf_search_button ); ?></button>
-		</form>
-	</div>
-
+		<button class="search-submit" type="submit"><?php echo esc_html( $wmf_search_button ); ?></button>
+	</form>
 </div>
+<button class="search-toggle">
+	<?php wmf_show_icon( 'search', 'material' ); ?>
+	<span class="search-label uppercase bold"><?php echo esc_html( $wmf_search_button ); ?></span>
+</button>
