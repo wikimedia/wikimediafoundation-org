23bdb03f6690a0b209b2305640f6a74baad06f4a
Nick Croft <nick@copyblogger.com>
2018-07-02 11:43:11 -0400

    enable RTL for flickity if enabled on site

--- themes/wmfoundation/functions.php
+++ themes/wmfoundation/functions.php
@@ -137,6 +137,12 @@ function wmf_scripts() {
 		)
 	);
 
+	wp_localize_script(
+		'wmfoundation-script', 'wmfrtl', array(
+			'enable' => get_theme_mod( 'wmf_enable_rtl' ) ? true : false,
+		)
+	);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
