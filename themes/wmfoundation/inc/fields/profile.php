c4c299c70f6c07e5ca1479e1d3d179921b049d47
Nick Croft <nick@copyblogger.com>
2018-07-10 12:15:53 -0400

    update context

--- themes/wmfoundation/inc/fields/profile.php
+++ themes/wmfoundation/inc/fields/profile.php
@@ -17,7 +17,7 @@ function wmf_profile_fields() {
 		)
 	);
 
-	$last_name->add_meta_box( __( 'Sorting', 'wmfoundation' ), 'profile', 'high' );
+	$last_name->add_meta_box( __( 'Sorting', 'wmfoundation' ), 'profile', 'normal', 'high' );
 
 	$contact_links = new Fieldmanager_Group(
 		array(
