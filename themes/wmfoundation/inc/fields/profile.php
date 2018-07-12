3e29fa4897f0fc7d180e3276c4142214a2e5bb2c
Jay Hoffmann <jhoffm34@gmail.com>
2018-07-12 08:11:03 -0400

    Moving action back to fieldmanager

--- themes/wmfoundation/inc/fields/profile.php
+++ themes/wmfoundation/inc/fields/profile.php
@@ -61,7 +61,7 @@ function wmf_profile_fields() {
 	);
 	$user->add_meta_box( __( 'Connected User', 'wmfoundation' ), 'profile' );
 }
-add_action( 'init', 'wmf_profile_fields', 101 );
+add_action( 'fm_post_profile', 'wmf_profile_fields', 101 );
 
 /**
  * Add fields for the role taxonomy
