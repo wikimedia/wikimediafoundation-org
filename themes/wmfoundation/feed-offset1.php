36012ce3fc58277aca0d3a17bf21acbba809570f
Gregory Varnum <gvarnum@wikimedia.org>
2019-02-28 01:50:29 -0800

    removal of global override
    Not needed or allowed

--- themes/wmfoundation/feed-offset1.php
+++ themes/wmfoundation/feed-offset1.php
@@ -7,7 +7,6 @@
  */
 
 header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
-$more = 1;
 
 echo '<?xml version="1.0" encoding="'.esc_attr(get_option('blog_charset')).'"?'.'>';
 
