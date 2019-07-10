3b0f3d300cbb7a7db9fdecc0d58bbcf09d58c48b
Gregory Varnum <gvarnum@wikimedia.org>
2019-07-10 02:59:58 -0700

    Update vision.php

--- themes/shiro/template-parts/header/vision.php
+++ themes/shiro/template-parts/header/vision.php
@@ -29,6 +29,6 @@ if (empty($visions)) {
 
 $is_visible = 'is_visible';
 foreach( array_combine($visions, $visions_rtl) as $vision => $vision_rtl ) {
-  echo '<h1 class="vision '. esc_attr($is_visible), esc_attr($vision_rtl) .'">' . esc_html($vision), esc_html($vision_rtl) . '</h1>';
+  echo '<h1 class="vision '. esc_attr($is_visible), esc_attr($vision_rtl) .'">' . esc_html($vision) . '</h1>';
   $is_visible = '';
 }
