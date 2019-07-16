7e343b2cb991b2d62a647e3c0127f96c38ba92a4
Gregory Varnum <gvarnum@wikimedia.org>
2019-07-16 00:49:31 -0700

    Revert "reset if same id"
    This reverts commit 479772ca91db85a48d7303d495109213c6053a70.

--- themes/shiro/inc/template-translations.php
+++ themes/shiro/inc/template-translations.php
@@ -137,9 +137,6 @@ function wmf_get_random_translation( $key, $args = array() ) {
 
 	$rand_key           = array_rand( $translations );
 	$target_translation = $translations[ $rand_key ];
-    if ($target_translation['site_id'] == $translations ['site_id']) {
-    	$target_translation = $translations[ $rand_key ];
-    }
 	$content_id         = $target_translation['content_id'];
 
 	switch_to_blog( $target_translation['site_id'] );
