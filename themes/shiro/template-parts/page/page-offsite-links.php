2c9093c81cccc78cdacd3108f2de22b09e98d41a
Hang Do Thi Duc <>
2020-04-03 11:19:36 -0700

    fields and template parts for modules in data page template

--- themes/shiro/template-parts/page/page-offsite-links.php
+++ themes/shiro/template-parts/page/page-offsite-links.php
@@ -11,7 +11,7 @@ $template_args          = get_post_meta( get_the_ID(), 'off_site_links', true );
 $template_args['split'] = false;
 $template               = is_singular( 'page' ) ? basename( get_page_template() ) : '';
 
-if ( ( 'page-landing.php' === $template ) || ( 'page-report.php' === $template ) ) {
+if ( ( 'page-landing.php' === $template ) || ( 'page-report.php' === $template ) || ( 'page-data.php' === $template ) ) {
 	$template_args['split'] = true;
 }
 
