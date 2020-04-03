3bf6f959df52959ed2d0555927d4f621074fa008
Hang Do Thi Duc <>
2020-04-03 11:17:59 -0700

    fields and template parts for header in data page template

--- themes/shiro/inc/template-functions.php
+++ themes/shiro/inc/template-functions.php
@@ -44,6 +44,7 @@ function wmf_get_header_container_class() {
 			switch ( $template ) {
 				case 'page-landing.php':
 				case 'page-report.php':
+				case 'page-data.php':
 				case is_front_page():
 					$class .= ' featured-photo--content-left';
 					break;
