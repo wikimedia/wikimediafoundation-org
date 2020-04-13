9788b96ddd6d4c2165a1bb0c4c3fb3643ae039c0
Gregory Varnum <gvarnum@wikimedia.org>
2020-04-13 15:04:15 -0400

    testing on preprod

--- themes/shiro/header.php
+++ themes/shiro/header.php
@@ -35,14 +35,14 @@ $wmf_current_language_label = get_theme_mod( 'wmf_current_language_label', __( '
      partnerReferrer: 'wikimediafoundation', //@type {string}
      iframeHost: site_url( '/wp-content/themes/shiro/assets/dist/earthdaylive/' ), // @type {string}
      disableGoogleAnalytics: true, // @type {boolean}
-     alwaysShowWidget: false, // @type {boolean}
-     forceFullPageWidget: false, // @type {boolean}
+     alwaysShowWidget: true, // @type {boolean}
+     forceFullPageWidget: true, // @type {boolean}
      showCloseButtonOnFullPageWidget: true, // @type {boolean}
      footerDisplayStartDate: new Date(), //@ type {Date object}
      fullPageDisplayStartDate: new Date(2020, 3, 22), //@ type {Date object}
   };
 </script>
-<!--<script src="/wp-content/themes/shiro/assets/dist/earthdaylive/widget.js" async></script>-->
+<script src="/wp-content/themes/shiro/assets/dist/earthdaylive/widget.js" async></script>
 </head>
 
 <body <?php body_class(); ?>>
