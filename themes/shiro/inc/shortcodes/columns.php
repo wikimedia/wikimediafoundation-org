99ea062f4df1ac74c57d945c7139e5a289a943f3
K Adam White <kadamwhite@users.noreply.github.com>
2020-04-29 18:38:45 -0400

    Implement support for reverse column order on mobile

--- themes/shiro/inc/shortcodes/columns.php
+++ themes/shiro/inc/shortcodes/columns.php
@@ -34,13 +34,20 @@ add_shortcode( 'wmf_column', 'wmf_column_shortcode_callback' );
  * Define a [wmf_columns] wrapper shortcode that creates the HTML wrapper for one
  * or more [wmf_column] shortcode items.
  *
+ * Supports a "reverse" attribute which causes the columns to render in reverse
+ * order on mobile. Usage:
+ *
+ *     [wmf_columns reverse] ... [/wmf_columns]
+ *
  * @param array  $atts    Shortcode attributes array.
  * @param string $content Content wrapped by shortcode.
  * @return string Rendered shortcode output.
  */
 function wmf_columns_shortcode_callback( $atts = [], $content = '' ) {
+	$reverse = empty( $atts ) ? false : in_array( 'reverse', array_map( 'strtolower', $atts ), true );
 	$content = do_shortcode( $content );
 	$content = preg_replace( '/\s*<br\s*\/?>\s*/', '', $content );
-	return '<div class="columns-wrapper">' . wp_kses_post( $content ) . '</div>';
+	$classes = $reverse ? 'columns-wrapper columns-mobile-reverse' : 'columns-wrapper';
+	return '<div class="' . esc_attr( $classes ) . '">' . wp_kses_post( $content ) . '</div>';
 }
 add_shortcode( 'wmf_columns', 'wmf_columns_shortcode_callback' );
