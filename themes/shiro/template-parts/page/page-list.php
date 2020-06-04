2c3b7feda488b543c5a56f031868bdafe8c791ab
Paul de Wouters <paul@humanmade.com>
2020-06-04 10:24:34 +0100

    Extract attachment IDs from custom fields that don't trigger hook

--- themes/shiro/template-parts/page/page-list.php
+++ themes/shiro/template-parts/page/page-list.php
@@ -6,5 +6,8 @@
  */
 
 $template_args = get_post_meta( get_the_ID(), 'list', true );
+if ( ! empty( $template_args[0]['description'] ) ) {
+	\WMF\Images\Credits::get_instance( get_the_ID() )->set_images_from_content( $template_args[0]['description'] );
+}
 
 wmf_get_template_part( 'template-parts/modules/list/list', $template_args );
