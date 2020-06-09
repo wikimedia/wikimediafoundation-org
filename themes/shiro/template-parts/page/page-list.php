91fbfa51a9c232208fa0798d778a4ea67936c089
Paul de Wouters <paul@humanmade.com>
2020-06-09 09:56:32 +0100

    Account for multiple list sections

--- themes/shiro/template-parts/page/page-list.php
+++ themes/shiro/template-parts/page/page-list.php
@@ -5,9 +5,11 @@
  * @package shiro
  */
 
-$template_args = get_post_meta( get_the_ID(), 'list', true );
-if ( ! empty( $template_args[0]['description'] ) ) {
-	\WMF\Images\Credits::get_instance( get_the_ID() )->set_images_from_content( $template_args[0]['description'] );
+$template_args = (array) get_post_meta( get_the_ID(), 'list', true );
+foreach ( $template_args as $list_section ) {
+	if ( ! empty( $list_section['description'] ) ) {
+		\WMF\Images\Credits::get_instance( get_the_ID() )->set_images_from_content( $list_section['description'] );
+	}
 }
 
 wmf_get_template_part( 'template-parts/modules/list/list', $template_args );
