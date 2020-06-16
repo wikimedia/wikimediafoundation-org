fe381774bab28f02d3723a76e7f86f8fe47e95db
Paul de Wouters <paul@humanmade.com>
2020-06-16 11:28:18 +0100

    Fix escaping function

--- themes/shiro/searchform.php
+++ themes/shiro/searchform.php
@@ -10,7 +10,7 @@ $wmf_search_toggle      = get_theme_mod( 'wmf_search_toggle', __( 'Toggle search
 $wmf_search_placeholder = get_theme_mod( 'wmf_search_placeholder_copy', __( 'What are you looking for?', 'shiro' ) );
 ?>
 
-<button class="search-toggle" aria-label="<?php echo esc_html( $wmf_search_toggle ); ?>" data-micromodal-trigger="search-modal">
+<button class="search-toggle" aria-label="<?php echo esc_attr( $wmf_search_toggle ); ?>" data-micromodal-trigger="search-modal">
 	<span class="btn-label-a11y"><?php echo esc_html( $wmf_search_toggle ); ?></span>
 	<?php wmf_show_icon( 'search', 'material' ); ?>
 	<span class="search-label uppercase bold"><?php echo esc_html( $wmf_search_button ); ?></span>
