77683a90381d6b8e8d1319c6e25d956d06d1824b
Paul de Wouters <paul@humanmade.com>
2020-06-23 17:04:20 +0100

    Add aria hidden attribute to H1 tag

--- themes/shiro/template-parts/header/vision.php
+++ themes/shiro/template-parts/header/vision.php
@@ -6,46 +6,55 @@
  */
 
 $visions = [
-  get_theme_mod('wmf_vision_lang1'),
-  get_theme_mod('wmf_vision_lang2'),
-  get_theme_mod('wmf_vision_lang3'),
-  get_theme_mod('wmf_vision_lang4'),
-  get_theme_mod('wmf_vision_lang5')
+	get_theme_mod( 'wmf_vision_lang1' ),
+	get_theme_mod( 'wmf_vision_lang2' ),
+	get_theme_mod( 'wmf_vision_lang3' ),
+	get_theme_mod( 'wmf_vision_lang4' ),
+	get_theme_mod( 'wmf_vision_lang5' ),
 ];
-$visions = array_filter($visions);
+$visions = array_filter( $visions );
 
 $visions_class = [
-  get_theme_mod('wmf_vision_lang1_class'),
-  get_theme_mod('wmf_vision_lang2_class'),
-  get_theme_mod('wmf_vision_lang3_class'),
-  get_theme_mod('wmf_vision_lang4_class'),
-  get_theme_mod('wmf_vision_lang5_class')
+	get_theme_mod( 'wmf_vision_lang1_class' ),
+	get_theme_mod( 'wmf_vision_lang2_class' ),
+	get_theme_mod( 'wmf_vision_lang3_class' ),
+	get_theme_mod( 'wmf_vision_lang4_class' ),
+	get_theme_mod( 'wmf_vision_lang5_class' ),
 ];
 
 $visions_langcode = [
-  get_theme_mod('wmf_vision_lang1_langcode'),
-  get_theme_mod('wmf_vision_lang2_langcode'),
-  get_theme_mod('wmf_vision_lang3_langcode'),
-  get_theme_mod('wmf_vision_lang4_langcode'),
-  get_theme_mod('wmf_vision_lang5_langcode')
+	get_theme_mod( 'wmf_vision_lang1_langcode' ),
+	get_theme_mod( 'wmf_vision_lang2_langcode' ),
+	get_theme_mod( 'wmf_vision_lang3_langcode' ),
+	get_theme_mod( 'wmf_vision_lang4_langcode' ),
+	get_theme_mod( 'wmf_vision_lang5_langcode' ),
 ];
 
-if (empty($visions)) {
-  $visions[] = '<span>Imagine a world</span> in which every single human being can freely share in the sum of all knowledge.';
-  $visions_class[] = '';
-  $visions_langcode[] = 'en-US';
+if ( empty( $visions ) ) {
+	$visions[]          = '<span>Imagine a world</span> in which every single human being can freely share in the sum of all knowledge.';
+	$visions_class[]    = '';
+	$visions_langcode[] = 'en-US';
 }
 
-$is_visible = 'is_visible';
+foreach ( $visions as $key => $vision ) {
+	$vision_output = [
+		'text'        => $visions[ $key ],
+		'langcode'    => $visions_langcode[ $key ],
+		// If language to display does not match site locale, then hide from screen readers.
+		// Not ideal because we depend on user entering the correct lang code - would be better as a dropdown.
+		// current customizer options are 'en-US, es, ar, hi, fr'.
+		'aria_hidden' => substr( $visions_langcode[ $key ], 0, 2 ) !== substr( get_locale(), 0, 2 ),
+		'classes'     => [],
+	];
 
-foreach ($visions as $key => $vision) {
-
-  $vision_output[$vision] = [
-    'text' => $visions[$key],
-    'rssclass' => $visions_class[$key],
-    'langcode' => $visions_langcode[$key],
-  ];
-    
-  echo '<h1 lang="'. esc_attr($vision_output[$vision]['langcode']) .'" class="vision '. esc_attr($is_visible) .' '. esc_attr($vision_output[$vision]['rssclass']) .'">' . esc_html($vision_output[$vision]['text']) . '</h1>';
-  $is_visible = '';
+	$vision_output['classes'] = [
+		0 === $key ? 'is_visible' : '',
+		'vision',
+		$visions_class[ $key ],
+	];
+	?>
+	<h1 aria-hidden="<?php echo esc_attr( (bool) $vision_output['aria_hidden'] ? 'true' : 'false' ); ?>" lang="<?php echo esc_attr( $vision_output['langcode'] ); ?>" class="<?php echo implode( ' ', array_map( 'esc_attr', array_filter( $vision_output['classes'] ) ) ); ?>">
+		<?php echo esc_html( $vision_output['text'] ); ?>
+	</h1>
+	<?php
 }
