b9a585b751959af94e551be6229136ac5ba50d6f
01fade <01fade@users.noreply.github.com>
2020-09-23 12:09:21 -0700

    new freeform page template for using mainly shortcodes

---
+++ themes/shiro/page-freeform.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Freeform
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package shiro
+ */
+
+get_header();
+while ( have_posts() ) {
+	the_post();
+
+	// Page Header.
+	$parent_page   = wp_get_post_parent_id( get_the_ID() );
+	$template_args = array(
+		'h4_link'  => ! empty( $parent_page ) ? get_the_permalink( $parent_page ) : '',
+		'h4_title' => ! empty( $parent_page ) ? get_the_title( $parent_page ) : '',
+		'h2_title' => '',
+		'h1_title' => '',
+	);
+
+	wmf_get_template_part( 'template-parts/header/page-noimage', $template_args );
+
+	the_content();
+	get_template_part( 'template-parts/page/page', 'connect' );
+	?>
+
+	<?php
+}
+get_footer();
