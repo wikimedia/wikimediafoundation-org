9ea99b513adb825f39780b568d61a9c2ffb83de1
01fade <01fade@users.noreply.github.com>
2021-01-05 15:23:59 +0100

    css vertical space class, minor color change

--- themes/shiro/inc/shortcodes/wp20.php
+++ themes/shiro/inc/shortcodes/wp20.php
@@ -68,7 +68,7 @@ function wmf_story_carousel_callback( $atts = [], $content = '' ) {
 	ob_start();
 	?>
 
-	<div id="<?php echo esc_attr($atts['id']) ?>" class="mod-margin-bottom story-carousel-container mod-padding-vertical <?php echo esc_attr( $atts['class'] ) ?>">
+	<div id="<?php echo esc_attr($atts['id']) ?>" class="mod-margin-bottom_sm story-carousel-container mod-padding-vertical_sm <?php echo esc_attr( $atts['class'] ) ?>">
 		<div class="mw-980 story-carousel-inner">
 			<div class="story-carousel w-68p">
 				<div class="story-content-container"><?php echo wp_kses_post( $content ) ?></div>
@@ -167,7 +167,7 @@ function wmf_recent_edits_callback( $atts = [], $content = '' ) {
 		<div class="mod-margin-bottom_xs"><?php echo wp_kses_post( $content ) ?></div>
 		<div id="<?php echo esc_attr($atts['id']) ?>" class="recent-edits mw-980">
 			<div class="rounded">
-				<div class="label-container"><span class="label"></span> <span class="wiki wp20-color-11"></span></div>
+				<div class="label-container"><span class="label"></span> <span class="wiki wp20-color-7"></span></div>
 				<div class="accent"><svg width="129" height="107" viewBox="0 0 129 107" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M126.08 1C118.84 27.21 97.28 49 71.14 56.48C66.06 57.93 60.4 58.83 55.66 56.48C50.92 54.13 48.22 47.33 51.66 43.33C54.66 39.86 60.66 40.48 63.94 43.62C67.22 46.76 68.49 51.55 68.83 56.11C69.3303 64.1928 67.6724 72.2633 64.0261 79.4943C60.3799 86.7252 54.8765 92.8566 48.08 97.26C41.1726 101.488 33.2655 103.804 25.1686 103.971C17.0716 104.138 9.07579 102.15 2 98.21" stroke="#000000" stroke-width="6" stroke-miterlimit="10"/></svg>
 				</div>
