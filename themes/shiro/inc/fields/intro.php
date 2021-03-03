787f17850ed540bc55c402015d154e231c5b378f
goldenapples <goldenapplesdesign@gmail.com>
2021-03-03 16:43:06 -0500

    Register page_intro field for the REST_API
    Registers this field in post meta using the core `register_meta` API in
    order for this field to be accessible inside Gutenberg using the REST
    schema.

--- themes/shiro/inc/fields/intro.php
+++ themes/shiro/inc/fields/intro.php
@@ -5,19 +5,73 @@
  * @package shiro
  */
 
+namespace WMF\Fields\Page_Intro;
+
+use Fieldmanager_RichTextArea;
+
 /**
- * Add intro options.
+ * Name of the meta field.
+ * @var string
+ */
+const FIELD_NAME = 'page_intro';
+
+/**
+ * Bootstrap functionality for this meta field.
+ */
+function bootstrap() {
+	add_action( 'init', __NAMESPACE__ . '\\register_meta_fields' );
+	//add_action( 'init', __NAMESPACE__ . '\\register_fieldmanager_fields' );
+}
+
+/**
+ * Register meta fields for use in the REST API.
+ */
+function register_meta_fields() {
+
+	$meta_args = [
+		'type'         => 'string',
+		'description'  => __( 'Intro field, displayed before post content in single views', 'shiro' ),
+		'single'       => true,
+		'show_in_rest' => true,
+	];
+
+	// The Intro field is available on all Posts.
+	register_post_meta( 'post', FIELD_NAME, $meta_args );
+
+	// This should only be used on Pages using the "report" template.
+	register_post_meta( 'page', FIELD_NAME, $meta_args );
+}
+
+/**
+ * Add Fieldmanager meta box for the classic editor.
+ *
+ * XXX: can be removed once the classic editor is no longer used.
+ */
+function register_fieldmanager_fields() {
+
+	add_action( 'fm_post_post', __NAMESPACE__ . '\\wmf_intro_fields', 5 );
+
+	if ( wmf_using_template( 'page-report' ) ) {
+		add_action( 'fm_post_page', 'wmf_intro_fields', 5 );
+	}
+}
+
+/**
+ * Add Fieldmanager meta box for the classic editor.
+ *
+ * XXX: can be removed once the classic editor is no longer used.
  */
 function wmf_intro_fields() {
+	$current_screen = get_current_screen();
+
+	if ( $current_screen->is_block_editor() ) {
+		return;
+	}
+
 	$intro = new Fieldmanager_RichTextArea(
 		array(
 			'name' => 'page_intro',
 		)
 	);
 	$intro->add_meta_box( __( 'Page Intro', 'shiro' ), array( 'post', 'page' ) );
-}
-add_action( 'fm_post_post', 'wmf_intro_fields', 5 );
-$is_report_page = wmf_using_template( 'page-report' );
-if ( $is_report_page ) {
-    add_action( 'fm_post_page', 'wmf_intro_fields', 5 );
 }
