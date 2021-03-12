836571c16abd4d2ac2c0d919849dffe76b5b935b
Anton Timmermans <email@atimmer.com>
2021-03-12 10:59:02 +0100

    Use ImagePicker component in card block

--- themes/shiro/assets/src/editor/blocks/card/index.js
+++ themes/shiro/assets/src/editor/blocks/card/index.js
@@ -1,14 +1,12 @@
 import {
 	InnerBlocks,
 	RichText,
-	MediaPlaceholder,
 	useBlockProps,
-	MediaReplaceFlow,
-	BlockControls,
 } from '@wordpress/block-editor';
-import { useSelect } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+
+import './style.scss';
+import ImagePicker from '../../components/image-picker';
 
 const template = [
 	[ 'core/heading', { level: 3 } ],
@@ -45,60 +43,9 @@ export const
 		/**
 		 * Render edit of the card block.
 		 */
-		edit: function EditCardBlock( { attributes, setAttributes, noticeUI, noticeOperations } ) {
+		edit: function EditCardBlock( { attributes, setAttributes } ) {
 			const blockProps = useBlockProps();
-			const { id } = attributes;
-
-			/**
-			 * Save selected image to the attributes.
-			 */
-			const handleSelectImage = media => {
-				if ( ! media || ! media.url ) {
-					setAttributes( {
-						imageUrl: undefined,
-						imageAlt: undefined,
-						id: undefined,
-					} );
-					return;
-				}
-
-				setAttributes( {
-					id: media.id,
-					imageAlt: media.alt,
-					imageUrl: media.url,
-				} );
-			};
-
-			/**
-			 * Handle an upload error
-			 */
-			const handleUploadError = message => {
-				noticeOperations.removeAllNotices();
-				noticeOperations.createErrorNotice( message );
-			};
-
-			/**
-			 * Retrieve the right image size from the media store.
-			 */
-			const imageUrl = useSelect( select => {
-				let media = select( 'core' ).getMedia( id );
-				return media?.media_details.sizes.image_16x9_small?.source_url || media?.source_url || attributes.imageUrl;
-			} );
-
-			useEffect( () => {
-				if ( imageUrl !== attributes.imageUrl ) {
-					setAttributes( { imageUrl } );
-				}
-			} );
-
-			const mediaPreview = !! imageUrl && (
-				<img
-					alt={ __( 'Edit image' ) }
-					className={ 'wp-block-shiro-card__image' }
-					src={ imageUrl }
-					title={ __( 'Edit image' ) }
-				/>
-			);
+			const { content } = attributes;
 
 			return (
 				<div { ...blockProps }>
@@ -106,40 +53,16 @@ export const
 						template={ template }
 						templateLock="all"
 					/>
-					<div className="wp-block-shiro-card__image-preview">
-						{ mediaPreview }
-						<MediaPlaceholder
-							accept="image/*"
-							allowedTypes={ [ 'image' ] }
-							disableMediaButtons={ imageUrl }
-							mediaPreview={ mediaPreview }
-							notices={ noticeUI }
-							value={ {
-								id,
-								src: imageUrl,
-							} }
-							onError={ handleUploadError }
-							onSelect={ handleSelectImage }
-						/>
-						<BlockControls>
-							{ !! imageUrl && (
-								<MediaReplaceFlow
-									accept="image/*"
-									allowedTypes={ [ 'image' ] }
-									mediaId={ id }
-									mediaURL={ imageUrl }
-									name={ __( 'Replace image', 'shiro' ) }
-									onError={ handleUploadError }
-									onSelect={ handleSelectImage }
-								/>
-							) }
-						</BlockControls>
-					</div>
+					<ImagePicker
+						attributes={ attributes }
+						defaultSize="image_16x9_small"
+						setAttributes={ setAttributes }
+					/>
 					<RichText
 						className="wp-block-shiro-card__body"
 						placeholder={ __( 'Start writing your card contents', 'shiro' ) }
 						tagName="p"
-						value={ attributes.content }
+						value={ content }
 						onChange={ content => setAttributes( { content } ) }
 					/>
 				</div>
@@ -151,14 +74,13 @@ export const
 		 */
 		save: function SaveCardBlock( { attributes } ) {
 			const blockProps = useBlockProps.save();
-			const { imageUrl } = attributes;
+			const { imageUrl, imageAlt, content } = attributes;
 
 			const image = !! imageUrl && (
 				<img
-					alt={ __( 'Edit image' ) }
+					alt={ imageAlt }
 					className={ 'wp-block-shiro-card__image' }
 					src={ imageUrl }
-					title={ __( 'Edit image' ) }
 				/>
 			);
 
@@ -169,7 +91,7 @@ export const
 					<RichText.Content
 						className="wp-block-shiro-card__body"
 						tagName="p"
-						value={ attributes.content }
+						value={ content }
 					/>
 				</div>
 			);
