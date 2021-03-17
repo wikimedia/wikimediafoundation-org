2542382b2de3d29ef75328772d70a5a9c4f423d6
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-03-17 16:05:28 -0700

    Add cta component
    (cherry picked from commit b0d39226d1902d6926c83fe4adf10e8882c48d83)

---
+++ themes/shiro/assets/src/editor/components/cta/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { RichText } from '@wordpress/block-editor';
+import { withFocusOutside } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+import URLPicker from '../url-picker';
+
+const CtaWithFocusOutside = withFocusOutside(
+	class extends React.Component {
+		constructor( props ) {
+			super( props );
+			this.state = {
+				showButtons: false,
+			};
+		}
+
+		handleFocusOutside() {
+			this.setState( { showButtons: false } );
+		}
+
+		render() {
+			const { showButtons } = this.state;
+			const {
+				text,
+				setAttributes,
+				url,
+			} = this.props;
+
+			return (
+				<>
+					<URLPicker
+						isSelected={ showButtons }
+						setAttributes={ setAttributes }
+						url={ url }
+					/>
+					<RichText
+						// For some reason withoutInteractiveFormatting doesn't
+						// work here, but this does.
+						allowedFormats={ [] }
+						className="banner__cta btn btn-blue"
+						placeholder={ __( 'Call to action', 'shiro' ) }
+						tagName="div"
+						value={ text }
+						onChange={ text => setAttributes( { buttonText: text } ) }
+						onFocus={ () => this.setState( { showButtons: true } ) }
+					/>
+				</>
+			);
+		}
+	}
+);
+
+/**
+ *
+ */
+CtaWithFocusOutside.Content = ( { url, text, className, ...props } ) => {
+	if ( ! url ) {
+		return null;
+	}
+
+	return (
+		<a
+			className={ className }
+			href={ url }
+			{ ...props }>{ text }</a>
+	);
+};
+
+export default CtaWithFocusOutside;
