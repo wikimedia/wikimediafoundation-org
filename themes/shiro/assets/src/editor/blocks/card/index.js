17d089c4b5ad6650f49d8bd7f1b715adb3903918
Anton Timmermans <email@atimmer.com>
2021-03-21 13:21:28 +0100

    Add first version of card block
    In combination with the columns block this can create the multiple
    cards that are shown on the homepage.

---
+++ themes/shiro/assets/src/editor/blocks/card/index.js
@@ -0,0 +1,181 @@
+/**
+ * Card block that makes a call to action with an image and text.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useCallback } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+import CallToActionPicker from '../../components/cta';
+import ImagePicker from '../../components/image-picker';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+export const name = 'shiro/card';
+
+export const settings = {
+	apiVersion: 2,
+
+	title: __( 'Card', 'shiro' ),
+
+	description: __(
+		'Card creates a call to action with an image, heading and paragraph.',
+		'shiro'
+	),
+
+	attributes: {
+		imageId: {
+			type: 'number',
+		},
+		imageSrc: {
+			type: 'string',
+			source: 'attribute',
+			selector: '.new-card__image',
+			attribute: 'src',
+		},
+		imageAlt: {
+			type: 'string',
+			source: 'attribute',
+			selector: '.new-card__image',
+			attribute: 'alt',
+		},
+		heading: {
+			type: 'string',
+			source: 'html',
+			selector: '.new-card__heading',
+		},
+		body: {
+			type: 'string',
+			source: 'html',
+			selector: '.new-card__body',
+		},
+		linkText: {
+			type: 'string',
+			source: 'html',
+			selector: '.new-card__cta',
+		},
+		linkUrl: {
+			type: 'string',
+			source: 'attribute',
+			selector: '.new-card__cta',
+			attribute: 'href',
+		},
+	},
+
+	/*
+ 		imageUrl: {
+ 			type: 'string',
+ 			source: 'attribute',
+ 			selector: '.hero__image',
+ 			attribute: 'src',
+ 		},
+ 		buttonText: {
+ 			type: 'string',
+ 			source: 'html',
+ 			selector: '.hero__cta',
+ 		},
+ 		buttonLink: {
+ 			type: 'string',
+ 			source: 'attribute',
+ 			selector: '.hero__cta',
+ 			attribute: 'href',
+ 		},
+	 */
+
+	/**
+	 * Edit component used to manage featured image and page intro.
+	 */
+	edit: function CardBlock( { attributes, setAttributes } ) {
+		const blockProps = useBlockProps( { className: 'new-card' } );
+		const { imageId, imageSrc, heading, body, linkText, linkUrl } = attributes;
+
+		const onSelectImage = useCallback( ( { id, src, alt } ) => {
+			setAttributes( {
+				imageId: id,
+				imageSrc: src,
+				imageAlt: alt,
+			} );
+		}, [ setAttributes ] );
+
+		return (
+			<div { ...blockProps }>
+				<ImagePicker
+					className="new-card__image"
+					id={ imageId }
+					imageSize="image_16x9_small"
+					src={ imageSrc }
+					onChange={ onSelectImage }
+				/>
+				<div className="new-card__contents">
+					<RichText
+						className="new-card__heading is-style-h3"
+						keepPlaceholderOnFocus
+						placeholder={ __( 'Heading of the card', 'shiro' ) }
+						tagName="h2"
+						value={ heading }
+						onChange={ heading => setAttributes( { heading } ) }
+					/>
+					<RichText
+						className="new-card__body has-small-font-size"
+						keepPlaceholderOnFocus
+						placeholder={ __( 'Body of the card', 'shiro' ) }
+						tagName="p"
+						value={ body }
+						onChange={ body => setAttributes( { body } ) }
+					/>
+					<CallToActionPicker
+						className="new-card__cta arrow-link"
+						text={ linkText }
+						url={ linkUrl }
+						onChangeLink={ linkUrl => setAttributes( { linkUrl } ) }
+						onChangeText={ linkText => setAttributes( { linkText } ) }
+					/>
+				</div>
+			</div>
+		);
+	},
+
+	/**
+	 * Render the frontend representation of the card block.
+	 */
+	save: function Save( { attributes } ) {
+		const blockProps = useBlockProps.save( { className: 'new-card' } );
+		const { imageId, imageSrc, heading, body, linkText, linkUrl } = attributes;
+
+		console.log( 'Saving attrs', attributes );
+
+		return (
+			<div { ...blockProps }>
+				<ImagePicker.Content
+					className="new-card__image"
+					id={ imageId }
+					imageSize="image_16x9_small"
+					src={ imageSrc }
+				/>
+				<div className="new-card__contents">
+					<RichText.Content
+						className="new-card__heading is-style-h3"
+						tagName="h2"
+						value={ heading }
+					/>
+					<RichText.Content
+						className="new-card__body has-small-font-size"
+						tagName="p"
+						value={ body }
+					/>
+					<CallToActionPicker.Content
+						className="new-card__cta"
+						text={ linkText }
+						url={ linkUrl }
+					/>
+				</div>
+			</div>
+		);
+	},
+};
