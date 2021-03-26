59698951ea2994408b8fb222e1432f3d68e2c90d
Anton Timmermans <email@atimmer.com>
2021-03-26 09:21:48 +0100

    Update cards block pattern after classname change

--- themes/shiro/inc/editor/patterns.php
+++ themes/shiro/inc/editor/patterns.php
@@ -171,20 +171,20 @@ CONTENT,
 		'content' => <<<CONTENT
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:shiro/card {"imageId":1} -->
-<div class="wp-block-shiro-card new-card click-to-call-to-action"><img class="wp-image-1 size-image_16x9_small new-card__image" src="https://s.w.org/images/core/5.3/MtBlanc1.jpg"/><div class="new-card__contents"><h2 class="new-card__heading is-style-h3">Research</h2><p class="new-card__body has-small-font-size">We conduct our own research and partner with researchers worldwide to address change in society and technology.</p><a class="new-card__cta call-to-action" href="https://google.com">More about research</a></div></div>
+<div class="wp-block-column"><!-- wp:shiro/card {"imageId":0} -->
+<div class="wp-block-shiro-card content-card click-to-call-to-action"><div class="content-card__contents"><h2 class="content-card__heading is-style-h3">Research</h2><p class="content-card__body has-small-font-size">We conduct our own research and partner with researchers worldwide to address change in society and technology.</p><a class="content-card__call-to-action call-to-action" href="https://wikimediafoundation.org/">More about research</a></div><img class="size-image_16x9_small content-card__image" src="https://s.w.org/images/core/5.3/MtBlanc1.jpg"/></div>
 <!-- /wp:shiro/card --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:shiro/card {"imageId":2} -->
-<div class="wp-block-shiro-card new-card click-to-call-to-action"><img class="wp-image-2 size-image_16x9_small new-card__image" src="https://s.w.org/images/core/5.3/Sediment_off_the_Yucatan_Peninsula.jpg"/><div class="new-card__contents"><h2 class="new-card__heading is-style-h3">Technology</h2><p class="new-card__body has-small-font-size">From site reliability to machine learning, our open-source technology makes Wikipedia faster, more reliable, and more accessible worldwide.<br>New line</p><a class="new-card__cta call-to-action" href="https://humanmade.com">More about technology</a></div></div>
+<div class="wp-block-column"><!-- wp:shiro/card {"imageId":0} -->
+<div class="wp-block-shiro-card content-card click-to-call-to-action"><div class="content-card__contents"><h2 class="content-card__heading is-style-h3">Technology</h2><p class="content-card__body has-small-font-size">From site reliability to machine learning, our open-source technology makes Wikipedia faster, more reliable, and more accessible worldwide.</p><a class="content-card__call-to-action call-to-action" href="https://wikimediafoundation.org/">More about technology</a></div><img class="size-image_16x9_small content-card__image" src="https://s.w.org/images/core/5.3/Sediment_off_the_Yucatan_Peninsula.jpg"/></div>
 <!-- /wp:shiro/card --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:shiro/card {"imageId":3} -->
-<div class="wp-block-shiro-card new-card click-to-call-to-action"><img class="wp-image-3 size-image_16x9_small new-card__image" src="https://s.w.org/images/core/5.3/Windbuchencom.jpg"/><div class="new-card__contents"><h2 class="new-card__heading is-style-h3">Advocacy</h2><p class="new-card__body has-small-font-size">We conduct our own research and partner with researchers worldwide to address change in society and technology.</p><a class="new-card__cta call-to-action" href="https://google.com">More about advocacy</a></div></div>
+<div class="wp-block-column"><!-- wp:shiro/card {"imageId":0} -->
+<div class="wp-block-shiro-card content-card click-to-call-to-action"><div class="content-card__contents"><h2 class="content-card__heading is-style-h3">Advocacy</h2><p class="content-card__body has-small-font-size">We conduct our own research and partner with researchers worldwide to address change in society and technology.</p><a class="content-card__call-to-action call-to-action" href="https://wikimediafoundation.org/">More about advocacy</a></div><img class="size-image_16x9_small content-card__image" src="https://s.w.org/images/core/5.3/Windbuchencom.jpg"/></div>
 <!-- /wp:shiro/card --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
