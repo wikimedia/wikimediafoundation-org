f79ea07cf5e42b5f6d050cbacc76ae89f2a86314
goldenapples <goldenapplesdesign@gmail.com>
2021-04-01 14:59:14 -0400

    Rename color support filter file and update docblock for clarity.
    This isn't a block, so it doesn't really make sense for it to live in
    the blocks directory, but it didn't seem like it made sense to create a
    new directory for categorization purposes. I'm just renaming it as
    `filter-color-support` for additional clarity of what it is.

--- themes/shiro/assets/src/editor/blocks/filter-color-support/index.js
+++ themes/shiro/assets/src/editor/blocks/filter-color-support/index.js
@@ -1,12 +1,17 @@
 /**
- * Remove support for picking text color from all blocks that support color pickers.
+ * Remove text color selection ability from all blocks with color pickers.
+ *
+ * Enforces that all content created uses the theme-defined color combinations,
+ * so that editors can't select a clashing or unreadable combination of text
+ * and background colors.
+ *
+ * The text color in all cases should be set intelligently based on the
+ * background color to preserve AAA contrast requirements for accessibility.
  */
 
 /**
  * Remove the "text" property from the block supports for all blocks that
- * support the color picker. The text color in all cases should be set
- * intelligently based on the background color to preserve AAA contrast
- * requirements for accessibility.
+ * support the color picker.
  *
  * @param {object} settings Block registration settings.
  * @param {string} name Block name.
