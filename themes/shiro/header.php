1e71a53339cdbc7dc0a658aa51a0c70dec3d3e33
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-02 16:25:31 -0700

    Better icon usage for language switcher

--- themes/shiro/header.php
+++ themes/shiro/header.php
@@ -67,19 +67,8 @@ $page_id = get_queried_object_id();
 						<div class="language-switcher" data-dropdown="language-switcher" data-open="false">
 							<button class="language-switcher__button" data-dropdown-toggle="language-switcher" aria-expanded="false">
 								<span class="btn-label-a11y"><?php echo esc_html( $wmf_current_language_label ); ?> </span>
-								<svg class="language-switcher__icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-									<title>
-										language
-									</title>
-									<path d="M20 18h-1.44a.61.61 0 0 1-.4-.12.81.81 0 0 1-.23-.31L17 15h-5l-1 2.54a.77.77 0 0 1-.22.3.59.59 0 0 1-.4.14H9l4.55-11.47h1.89zm-3.53-4.31L14.89 9.5a11.62 11.62 0 0 1-.39-1.24q-.09.37-.19.69l-.19.56-1.58 4.19zm-6.3-1.58a13.43 13.43 0 0 1-2.91-1.41 11.46 11.46 0 0 0 2.81-5.37H12V4H7.31a4 4 0 0 0-.2-.56C6.87 2.79 6.6 2 6.6 2l-1.47.5s.4.89.6 1.5H0v1.33h2.15A11.23 11.23 0 0 0 5 10.7a17.19 17.19 0 0 1-5 2.1q.56.82.87 1.38a23.28 23.28 0 0 0 5.22-2.51 15.64 15.64 0 0 0 3.56 1.77zM3.63 5.33h4.91a8.11 8.11 0 0 1-2.45 4.45 9.11 9.11 0 0 1-2.46-4.45z" fill="currentColor"/>
-								</svg>
+								<?php echo wmf_show_icon( 'translate', 'language-switcher__icon' ); ?>
 								<span class="language-switcher__label"><?php echo $selected; ?></span>
-								<svg xmlns="http://www.w3.org/2000/svg" class="language-switcher__caret" width="12" height="12" viewBox="0 0 12 12">
-									<title>
-										down
-									</title>
-									<path d="M10.085 2.943L6.05 6.803l-3.947-3.86L1.05 3.996l5 5 5-5z" fill="currentColor"/>
-								</svg>
 							</button>
 							<div data-dropdown-content class="language-switcher__content" hidden>
 								<ul>
