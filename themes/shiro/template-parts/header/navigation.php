3009e1e2b00552f02cec9b69e149200f3b72aa39
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-05 14:39:29 -0700

    Prevent multi-level menus

--- themes/shiro/template-parts/header/navigation.php
+++ themes/shiro/template-parts/header/navigation.php
@@ -16,6 +16,11 @@
 				'theme_location' => 'header',
 				'menu_class'     => 'primary-nav__items',
 				'container'      => '',
+				// Remove this when implementing multi-leve navigation.
+				// For now it's here because the existing menu implementation
+				// doesn't super more than one level and looks very odd if
+				// you try.
+				'depth' => 1,
 			)
 		);
 	}
