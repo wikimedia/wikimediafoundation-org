0a3f0cdaf048a81f1e44fb95e7aac9b80f60b7e9
goldenapples <goldenapplesdesign@gmail.com>
2021-04-06 11:16:16 -0400

    Rename block pattern category to "Wikimedia"
    Makes the categorization here more parallel with the categories under
    blocks themselves.

--- themes/shiro/inc/editor/patterns.php
+++ themes/shiro/inc/editor/patterns.php
@@ -6,6 +6,11 @@
 namespace WMF\Editor\Patterns;
 
 /**
+ * @var string The slug for the block pattern category to group these into.
+ */
+const CATEGORY_NAME = 'wikimedia';
+
+/**
  * Hook into WordPress
  */
 function bootstrap() {
@@ -13,32 +18,32 @@ function bootstrap() {
 }
 
 function register_pattern() {
-	register_block_pattern_category( 'wikimedia-columns', [
-		'label' => __( 'Wikimedia columns', 'shiro' ),
+	register_block_pattern_category( CATEGORY_NAME, [
+		'label' => __( 'Wikimedia', 'shiro' ),
 	] );
 
 	register_block_pattern( FactColumns\NAME, [
 		'title' => __( 'Numbered fact columns' ),
-		'categories' => [ 'wikimedia-columns' ],
+		'categories' => [ CATEGORY_NAME ],
 		'content' => FactColumns\PATTERN,
 	] );
 
 	register_block_pattern( TweetColumns\NAME, [
 		'title' => __( 'Tweet this columns', 'shiro' ),
-		'categories' => [ 'wikimedia-columns' ],
+		'categories' => [ CATEGORY_NAME ],
 		'content' => TweetColumns\PATTERN,
 
 	] );
 
 	register_block_pattern( CardColumns\NAME, [
 		'title' => __( 'Cards' ),
-		'categories' => [ 'wikimedia-columns' ],
+		'categories' => [ CATEGORY_NAME ],
 		'content' => CardColumns\PATTERN,
 	] );
 
 	register_block_pattern( BlogList\NAME, [
 		'title' => __( 'Blog list section', 'shiro' ),
-		'categories' => [ 'wikimedia-columns' ],
+		'categories' => [ CATEGORY_NAME ],
 		'content' => BlogList\PATTERN
 	] );
 
