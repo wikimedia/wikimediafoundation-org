3d8d8073e94ebdf7dabfc5ddf4755ea9a8c97db4
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-07 15:41:04 -0700

    Add js implementation for inline-languages block

---
+++ themes/shiro/assets/src/editor/blocks/inline-languages/index.js
@@ -0,0 +1,46 @@
+/**
+ * Block for inserting links to translated content.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import {
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import ServerSideRender from '@wordpress/server-side-render';
+
+export const
+	name = 'shiro/inline-languages',
+	settings = {
+		apiVersion: 2,
+		title: __( 'Inline Languages', 'shiro' ),
+		supports: {
+			align: [ 'center', 'full' ],
+		},
+		/**
+		 *
+		 */
+		edit: function EditInlineLanguagesBlock( { attributes, setAttributes } ) {
+			const blockProps = useBlockProps();
+			const { align } = attributes;
+
+			return (
+				<div { ...blockProps }>
+					<ServerSideRender
+						attributes={ attributes }
+						block={ name }
+					/>
+				</div>
+			);
+		},
+		/**
+		 * Save nothing, to allow for server-size rendering.
+		 *
+		 * @returns {null}
+		 */
+		save: function () {
+			return null;
+		},
+	};
