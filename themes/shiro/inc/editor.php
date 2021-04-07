4169a0765345f1780d5a060acb39eef07cf49d3c
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-07 15:42:32 -0700

    Specifically allow inline-languages block to be used

--- themes/shiro/inc/editor.php
+++ themes/shiro/inc/editor.php
@@ -54,6 +54,7 @@ function filter_blocks( $allowed_blocks ) {
 		'shiro/stair',
 		'shiro/landing-page-hero',
 		'shiro/mailchimp-subscribe',
+		'shiro/inline-languages',
 
 		// Core blocks
 		'core/paragraph',
