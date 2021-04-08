55d231a19657db8628b515b086dac661a0c6428b
goldenapples <goldenapplesdesign@gmail.com>
2021-04-08 17:41:35 -0400

    Install wp-pot-cli and babel-plugin-makepot
    Adds the npm, packages required for generating pot files for both PHP
    and javascript files, and adds a command to the package.json to run
    them.

--- themes/shiro/.babelrc.js
+++ themes/shiro/.babelrc.js
@@ -1,1 +1,16 @@
-module.exports = require( '@humanmade/webpack-helpers/babel-preset' );
+module.exports = ( api ) => {
+	api.cache.forever();
+
+	return {
+		presets: [ '@wordpress/default' ],
+		plugins: [
+			'@babel/plugin-proposal-class-properties',
+			[ 'transform-react-jsx', {
+				pragma: 'wp.element.createElement',
+			} ],
+			[ "@wordpress/babel-plugin-makepot", {
+				"output": "languages/shiro-js.pot"
+			} ]
+		],
+	};
+};
