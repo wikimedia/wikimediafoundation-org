9c443b085923b0a5b683e4673c84f4706fc9fc86
Anton Timmermans <email@atimmer.com>
2021-04-08 10:19:08 +0200

    Implement tweet-this block
    This is a simple RichText to set the text and inspector controls to
    set the tweet text and URL. I believe we need a text-only button in
    other locations in the project, so I thought I would introduce it as a
    style on the button.

---
+++ themes/shiro/assets/src/editor/blocks/tweet-this/index.js
@@ -0,0 +1,104 @@
+import { isString } from 'lodash';
+
+import { InspectorControls } from '@wordpress/block-editor';
+import {
+	RichText,
+	useBlockProps,
+	URLInput,
+} from '@wordpress/block-editor';
+import { TextareaControl, PanelBody } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+import './style.scss';
+
+export const
+	name = 'shiro/tweet-this',
+	settings = {
+		apiVersion: 2,
+		title: __( 'Tweet this', 'shiro' ),
+		category: 'wikimedia',
+		attributes: {
+			text: {
+				type: 'string',
+				source: 'html',
+				selector: '.tweet-this',
+			},
+			tweetText: {
+				type: 'string',
+			},
+			tweetUrl: {
+				type: 'string',
+			},
+		},
+
+		/**
+		 * Render edit of the tweet this block.
+		 */
+		edit: function EditTweetThis( { attributes, setAttributes } ) {
+			const blockProps = useBlockProps( { className: 'tweet-this' } );
+			const { text, tweetText, tweetUrl } = attributes;
+
+			const permalink = useSelect( select => select( 'core/editor' ).getPermalink() );
+			useEffect( () => {
+				if ( ! isString( tweetUrl ) ) {
+					setAttributes( { tweetUrl: permalink } );
+				}
+			}, [ tweetUrl, permalink, setAttributes ] );
+
+			return (
+				<div { ...blockProps }>
+					<RichText
+						className="wp-block-shiro-button is-style-as-link has-icon has-icon-social-twitter-blue"
+						keepPlaceholderOnFocus
+						placeholder={ __( 'Write tweet this text', 'shiro' ) }
+						tagName="div"
+						value={ text }
+						onChange={ text => setAttributes( { text } ) }
+					/>
+					<InspectorControls>
+						<PanelBody title={ __( 'Tweet settings', 'shiro' ) }>
+							<TextareaControl
+								help={ __( 'When clicking the link, this text will be inside the composed tweet', 'shiro' ) }
+								label={ __( 'Tweet text', 'shiro' ) }
+								value={ tweetText }
+								onChange={ tweetText => setAttributes( { tweetText } ) }
+							/>
+							<URLInput
+								className="tweet-this__url-input"
+								isFullWidth
+								label={ __( 'Tweet URL', 'shiro' ) }
+								value={ tweetUrl }
+								onChange={ tweetUrl => setAttributes( { tweetUrl } ) }
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</div>
+			);
+		},
+
+		/**
+		 * Render save of the tweet this block.
+		 */
+		save: function SaveStairBlock( { attributes } ) {
+			const blockProps = useBlockProps.save( { className: 'tweet-this' } );
+			const { text } = attributes;
+			let { tweetText, tweetUrl } = attributes;
+
+			tweetText = encodeURIComponent( tweetText );
+			tweetUrl = encodeURIComponent( tweetUrl );
+
+			const tweetThisUrl = `https://twitter.com/intent/tweet?text=${ tweetText } ${ tweetUrl }`;
+
+			return (
+				<RichText.Content
+					href={ tweetThisUrl }
+					tagName="a"
+					value={ text }
+					{ ...blockProps }
+					className="tweet-this wp-block-shiro-button is-style-as-link has-icon has-icon-social-twitter-blue"
+				/>
+			);
+		},
+	};
--- themes/shiro/inc/editor.php
+++ themes/shiro/inc/editor.php
@@ -52,6 +52,7 @@ function filter_blocks( $allowed_blocks ) {
 		'shiro/card',
 		'shiro/stairs',
 		'shiro/stair',
+		'shiro/tweet-this',
 		'shiro/landing-page-hero',
 		'shiro/mailchimp-subscribe',
 
