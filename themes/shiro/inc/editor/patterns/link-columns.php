773f4487155aa24478760e3d4e302e4229e3edd1
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-15 22:12:54 +0000

    Build changes from 5fcf724555d40f6f287dc5b445c495fb5ef3571f
    Update link-columns pattern with correct html

--- themes/shiro/inc/editor/patterns/link-columns.php
+++ themes/shiro/inc/editor/patterns/link-columns.php
@@ -11,26 +11,27 @@ const PATTERN = <<<CONTENT
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:shiro/external-link -->
-<div class="wp-block-shiro-external-link external-link"></div>
+<div class="wp-block-shiro-external-link external-link"><p class="external-link__heading"><a class="external-link__link"><span class="external-link__heading-text"></span><svg class="icon-open external-link__icon"><use href="http://wikimediafoundation.test/wp-content/themes/shiro/assets/dist/icons.svg#open"></use></svg></a></p><p class="external-link__text"></p></div>
 <!-- /wp:shiro/external-link --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:shiro/external-link -->
-<div class="wp-block-shiro-external-link external-link"></div>
+<div class="wp-block-shiro-external-link external-link"><p class="external-link__heading"><a class="external-link__link"><span class="external-link__heading-text"></span><svg class="icon-open external-link__icon"><use href="http://wikimediafoundation.test/wp-content/themes/shiro/assets/dist/icons.svg#open"></use></svg></a></p><p class="external-link__text"></p></div>
 <!-- /wp:shiro/external-link --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
+
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:shiro/external-link -->
-<div class="wp-block-shiro-external-link external-link"></div>
+<div class="wp-block-shiro-external-link external-link"><p class="external-link__heading"><a class="external-link__link"><span class="external-link__heading-text"></span><svg class="icon-open external-link__icon"><use href="http://wikimediafoundation.test/wp-content/themes/shiro/assets/dist/icons.svg#open"></use></svg></a></p><p class="external-link__text"></p></div>
 <!-- /wp:shiro/external-link --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:shiro/external-link -->
-<div class="wp-block-shiro-external-link external-link"></div>
+<div class="wp-block-shiro-external-link external-link"><p class="external-link__heading"><a class="external-link__link"><span class="external-link__heading-text"></span><svg class="icon-open external-link__icon"><use href="http://wikimediafoundation.test/wp-content/themes/shiro/assets/dist/icons.svg#open"></use></svg></a></p><p class="external-link__text"></p></div>
 <!-- /wp:shiro/external-link --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
