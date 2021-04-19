ae40c6d3ea338ba90261a12e5a82f06bc24280f8
Anton Timmermans <email@atimmer.com>
2021-04-19 09:35:56 +0200

    Update tweet columns with new tweet-this block

--- themes/shiro/inc/editor/patterns/tweet-columns.php
+++ themes/shiro/inc/editor/patterns/tweet-columns.php
@@ -7,22 +7,35 @@ namespace WMF\Editor\Patterns\TweetColumns;
 
 const NAME = 'shiro/tweet-columns';
 
-const PATTERN = <<<CONTENT
+/**
+ * Get the pattern content.
+ *
+ * Returned as a function because we need to dynamically generate a URL to use
+ * as the tweet URL. If we don't the tweet-this block will always fail block
+ * validation. That would show the user the invalid content warning.
+ *
+ * @return string
+ */
+function pattern() {
+	$post_id = $_GET['post'] ?? false;
+	$permalink = $post_id ? get_permalink( $post_id ) : get_home_url();
+	$permalink_encoded = urlencode( $permalink );
+
+	return <<<CONTENT
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"backgroundColor":"blue90"} -->
 <div class="wp-block-group has-blue-90-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:paragraph {"className":"is-style-h1"} -->
 <p class="is-style-h1">200,000+</p>
 <!-- /wp:paragraph -->
+
 <!-- wp:paragraph -->
 <p><strong>Editors contribute to Wikimedia projects every month</strong></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-tertiary has-icon has-icon-social-twitter-blue"} -->
-<div class="wp-block-button is-style-tertiary has-icon has-icon-social-twitter-blue"><a class="wp-block-button__link">Tweet this</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div></div>
+<!-- wp:shiro/tweet-this {"tweetText":"200,000+ editors contribute to Wikimedia projects every month", "tweetUrl":"$permalink"} -->
+<a href="https://twitter.com/intent/tweet?text=200%2C000%2B%20editors%20contribute%20to%20Wikimedia%20projects%20every%20month $permalink_encoded" class="tweet-this wp-block-shiro-button is-style-as-link has-icon has-icon-social-twitter-blue">Tweet this</a>
+<!-- /wp:shiro/tweet-this --></div></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
@@ -36,11 +49,9 @@ const PATTERN = <<<CONTENT
 <p><strong>Media files on Wikimedia Commons</strong></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-tertiary has-icon has-icon-social-twitter-blue"} -->
-<div class="wp-block-button is-style-tertiary has-icon has-icon-social-twitter-blue"><a class="wp-block-button__link">Tweet this</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div></div>
+<!-- wp:shiro/tweet-this {"tweetText":"68+ million media files on Wikimedia Commons", "tweetUrl":"$permalink"} -->
+<a href="https://twitter.com/intent/tweet?text=68%2B%20million%20media%20files%20on%20Wikimedia%20Commons $permalink_encoded" class="tweet-this wp-block-shiro-button is-style-as-link has-icon has-icon-social-twitter-blue">Tweet this</a>
+<!-- /wp:shiro/tweet-this --></div></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
@@ -54,12 +65,11 @@ const PATTERN = <<<CONTENT
 <p><strong>Unique devices access Wikimedia projects every month</strong></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-tertiary has-icon has-icon-social-twitter-blue"} -->
-<div class="wp-block-button is-style-tertiary has-icon has-icon-social-twitter-blue"><a class="wp-block-button__link">Tweet this</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div></div>
+<!-- wp:shiro/tweet-this {"tweetText":"1+ billion unique devices access Wikimedia projects every month", "tweetUrl":"$permalink"} -->
+<a href="https://twitter.com/intent/tweet?text=1%2B%20billion%20unique%20devices%20access%20Wikimedia%20projects%20every%20month $permalink_encoded" class="tweet-this wp-block-shiro-button is-style-as-link has-icon has-icon-social-twitter-blue">Tweet this</a>
+<!-- /wp:shiro/tweet-this --></div></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 CONTENT;
+}
