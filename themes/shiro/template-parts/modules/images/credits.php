a63c3edea8b168d267d3bbef2e509fe48b899b04
Anton Timmermans <email@atimmer.com>
2021-04-22 10:57:01 +0200

    Create new photo credits styling

--- themes/shiro/template-parts/modules/images/credits.php
+++ themes/shiro/template-parts/modules/images/credits.php
@@ -7,7 +7,7 @@
 
 $image_ids = $args['image_ids'];
 
-if ( empty( $images ) ) {
+if ( empty( $image_ids ) ) {
 	return;
 }
 
@@ -32,3 +32,17 @@ $header = get_theme_mod( 'wmf_image_credit_header', __( 'Photo credits', 'shiro'
 	</div>
 
 </div>
+
+<section class="photo-attribution">
+	<?php if ( ! empty( $header ) ) : ?>
+		<h2 class="photo-attribution__heading"><?php echo esc_html( $header ); ?></h2>
+	<?php endif; ?>
+
+	<div class="photo-attribution__inner">
+		<?php
+			foreach ( $image_ids as $image_id ) {
+				get_template_part( 'template-parts/modules/images/credit-new', null, [ 'image_id' => $image_id ] );
+			}
+		?>
+	</div>
+</section>
