96a881acfbfc6a074ab0c438c76f206d10cdd7b2
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-04-30 10:48:34 -0700

    Refactor sticky site header js
    This updates and relocates this code to our "modern" workflow, and also
    removes the old code. It also targets the new class.

---
+++ themes/shiro/assets/src/scripts/modules/sticky-site-header.js
@@ -0,0 +1,81 @@
+/**
+ * This module applies a class to the site header when it is not at the top
+ * of the viewport, allowing us to adjust styling and CSS behavior when it is
+ * at the top/not at the top.
+ */
+
+const siteHeaderSelector = 'site-header';
+const pinnedClass = `${siteHeaderSelector}--pinned`;
+
+/**
+ * This is set outside of any method to 'cache' its value.
+ *
+ * Don't access it directly unless you need to reset it to something else
+ * (i.e. you're modifying the DOM w/ JavaScript). Use its get method
+ * (getSiteHeaderBar) instead.
+ *
+ * @type {Element}
+ */
+let _siteHeaderBar = document.querySelector( `.${siteHeaderSelector}` );
+
+/**
+ * Get the element that is the site header.
+ *
+ * @returns {Element} The element that represents the site header.
+ */
+function getSiteHeaderBar() {
+	return _siteHeaderBar;
+}
+
+/**
+ * Handles everything this module needs to do when a scroll event happens.
+ */
+function handleScroll() {
+	const el = getSiteHeaderBar();
+	if ( document.documentElement.scrollTop > 0 ) {
+		el.classList.add( pinnedClass );
+	} else {
+		el.classList.remove( pinnedClass );
+	}
+}
+
+/**
+ * Run any tasks necessary to start up this module.
+ *
+ * @todo It would be nice if we could somehow avoid running this on *every* event while maintaining a smooth experience.
+ */
+function setup() {
+	window.addEventListener( 'scroll', handleScroll );
+}
+
+/**
+ * Remove the (side-)effects of this module, i.e. for HMR.
+ * Probably not used in production.
+ */
+function teardown() {
+	window.removeEventListener( 'scroll', handleScroll );
+	const el = getSiteHeaderBar();
+	if ( el ) {
+		el.classList.remove( pinnedClass );
+	}
+}
+
+/**
+ * Initialize module with HMR support.
+ *
+ * If you /just/ want to setup it up, import `setup`.
+ */
+function initialize() {
+	if ( module.hot ) {
+		module.hot.accept();
+		module.hot.dispose( teardown );
+		setup();
+	} else {
+		setup();
+	}
+}
+
+export default initialize;
+export {
+	teardown, setup,
+};
--- themes/shiro/assets/src/scripts/shiro.js
+++ themes/shiro/assets/src/scripts/shiro.js
@@ -2,7 +2,9 @@
 
 import './block-hero-home';
 import dropdown from './modules/dropdown';
+import stickySiteHeader from './modules/sticky-site-header';
 import vhFixer from './modules/vh-fixer';
 
 dropdown();
 vhFixer();
+stickySiteHeader();
