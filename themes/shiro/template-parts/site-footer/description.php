928a4136c1883f190bec633e70764ae7a5cf6a39
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-07 14:54:59 -0700

    Add class to description template

--- themes/shiro/template-parts/site-footer/description.php
+++ themes/shiro/template-parts/site-footer/description.php
@@ -4,8 +4,10 @@ $wmf_footer_text  = get_theme_mod( 'wmf_footer_text',
 	__( 'The Wikimedia Foundation, Inc is a nonprofit charitable organization dedicated to encouraging the growth, development and distribution of free, multilingual content, and to providing the full content of these wiki-based projects to the public free of charge.',
 		'shiro' ) );
 ?>
-
-<img class="site-footer__logo" src="<?php echo esc_url( $wmf_footer_image ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
-<?php if ( ! empty( $wmf_footer_text ) ) : ?>
-	<p><?php echo esc_html( $wmf_footer_text ); ?></p>
-<?php endif; ?>
+<div class="site-footer__description">
+	<img class="site-footer__logo" src="<?php echo esc_url( $wmf_footer_image ); ?>"
+		 alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
+	<?php if ( ! empty( $wmf_footer_text ) ) : ?>
+		<p><?php echo esc_html( $wmf_footer_text ); ?></p>
+	<?php endif; ?>
+</div>
