8a1c5eb6c23f14af95ca66e8b0d61c0d6574fed4
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-07 13:18:01 -0700

    Add some elementary positioning and colors

--- themes/shiro/template-parts/site-footer/wrapper.php
+++ themes/shiro/template-parts/site-footer/wrapper.php
@@ -1,9 +1,11 @@
 <?php
 
 ?>
-<footer>
-	<?php get_template_part( 'template-parts/site-footer/callout-menu' ); ?>
-	<?php get_template_part( 'template-parts/site-footer/description' ); ?>
-	<?php get_template_part( 'template-parts/site-footer/navigation' ); ?>
-	<?php get_template_part( 'template-parts/site-footer/legal' ); ?>
+<footer class="site-footer">
+	<div class="site-footer__inner">
+		<?php get_template_part( 'template-parts/site-footer/callout-menu' ); ?>
+		<?php get_template_part( 'template-parts/site-footer/description' ); ?>
+		<?php get_template_part( 'template-parts/site-footer/navigation' ); ?>
+		<?php get_template_part( 'template-parts/site-footer/legal' ); ?>
+	</div>
 </footer>
