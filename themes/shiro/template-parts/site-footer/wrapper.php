0ef795380695cc034766f27abe7eee2c0b99d54c
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-07 14:54:22 -0700

    Fix order of templates in footer wrapper

--- themes/shiro/template-parts/site-footer/wrapper.php
+++ themes/shiro/template-parts/site-footer/wrapper.php
@@ -3,8 +3,8 @@
 ?>
 <footer class="site-footer">
 	<div class="site-footer__inner">
-		<?php get_template_part( 'template-parts/site-footer/callout-menu' ); ?>
 		<?php get_template_part( 'template-parts/site-footer/description' ); ?>
+		<?php get_template_part( 'template-parts/site-footer/callout-nav' ); ?>
 		<?php get_template_part( 'template-parts/site-footer/navigation' ); ?>
 		<?php get_template_part( 'template-parts/site-footer/legal' ); ?>
 	</div>
