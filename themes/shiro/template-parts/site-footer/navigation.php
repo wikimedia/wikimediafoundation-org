5159d2ad9e0fc61898c8ea2818a99af3d76314f0
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-10 11:23:08 -0700

    Refactor footer navigation template

--- themes/shiro/template-parts/site-footer/navigation.php
+++ themes/shiro/template-parts/site-footer/navigation.php
@@ -4,58 +4,50 @@ $wmf_projects_menu_label            = get_theme_mod( 'wmf_projects_menu_label', 
 $wmf_movement_affiliates_menu_label = get_theme_mod( 'wmf_movement_affiliates_menu_label',
 	__( 'Movement Affiliates', 'shiro' ) );
 $wmf_other_links_menu_label         = get_theme_mod( 'wmf_other_links_menu_label', __( 'Other', 'shiro' ) );
-?>
+
+$default_args = [
+	'menu_class' => 'site-footer__navmenu',
+	'container'  => '',
+	'items_wrap' => '<nav class="%2$s"><ul>%3$s</ul></nav>',
+];
+
+$menus = [
+	[
+		'label'    => $wmf_projects_menu_label,
+		'location' => 'footer-projects',
+		'args'     => [
+			'theme_location' => 'footer-projects',
+		]
+	],
+	[
+		'label'    => $wmf_movement_affiliates_menu_label,
+		'location' => 'footer-affiliates',
+		'args'     => [
+			'theme_location' => 'footer-affiliates',
+		]
+	],
+	[
+		'label'    => $wmf_other_links_menu_label,
+		'location' => 'footer-legal',
+		'args'     => [
+			'theme_location' => 'footer-legal',
+		]
+	]
+]; ?>
+
 <div class="site-footer__navigation">
-	<div class="w-50p">
-		<h2 class="h3"><?php echo esc_html( $wmf_projects_menu_label ); ?></h2>
-		<?php
-		if ( has_nav_menu( 'footer-projects' ) ) {
-			wp_nav_menu(
-				array(
-					'theme_location' => 'footer-projects',
-					'menu_class'     => 'lists-wrap flex flex-all',
-					'container'      => '',
-					'items_wrap'     => '<div id="%1$s" class="%2$s"><ul class="w-32p">%3$s</ul></div>',
-					'walker'         => new WMF\Walkers\Columns(),
-				)
-			);
+
+	<?php foreach ( $menus as $menu ) {
+		if ( has_nav_menu( $menu['location'] ?? '' ) ) {
+			$args = array_merge( $default_args, $menu['args'] ?? [] );
+			?>
+			<div class="site-footer__navigation-section site-footer__navigation-section--<?php echo esc_attr( $menu['location'] ); ?>">
+				<h2><?php echo esc_html( $menu['label'] ); ?></h2>
+				<?php wp_nav_menu( $args ); ?>
+			</div>
+			<?php
 		}
-		?>
-	</div>
-
-	<div class="w-25p">
-		<h2 class="h3"><?php echo esc_html( $wmf_movement_affiliates_menu_label ); ?></h2>
-		<div class="lists-wrap">
-			<?php
-			if ( has_nav_menu( 'footer-affiliates' ) ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer-affiliates',
-						'menu_class'     => '',
-						'container'      => '',
-					)
-				);
-			}
-			?>
-		</div>
-	</div>
-
-	<div class="w-25p">
-		<h2 class="h3"><?php echo esc_html( $wmf_other_links_menu_label ); ?></h2>
-		<div class="lists-wrap">
-			<?php
-			if ( has_nav_menu( 'footer-legal' ) ) {
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer-legal',
-						'menu_class'     => '',
-						'container'      => '',
-					)
-				);
-			}
-			?>
-		</div>
-		<br>
-	</div>
+	}
+	?>
 
 </div>
