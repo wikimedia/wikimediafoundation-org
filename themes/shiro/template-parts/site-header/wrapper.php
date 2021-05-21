04c88cc7c0675104f8494e389f3dc4490136337c
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-21 16:43:22 -0700

    Add 'uninitialized' default to dropdowns

--- themes/shiro/template-parts/site-header/wrapper.php
+++ themes/shiro/template-parts/site-header/wrapper.php
@@ -8,7 +8,7 @@
 ?>
 
 <div class="site-header">
-	<div class="site-header__inner" data-dropdown="language-switcher" data-dropdown-toggle=".language-switcher__button"
+	<div class="site-header__inner" data-dropdown="language-switcher" data-dropdown-toggle=".language-switcher__button" data-dropdown-status="uninitialized"
 		 data-dropdown-content=".language-switcher__content"
 		 data-visible="no" data-trap="inactive" data-backdrop="inactive" data-toggleable="yes">
 		<?php get_template_part( 'template-parts/site-header/toggle' ); ?>
