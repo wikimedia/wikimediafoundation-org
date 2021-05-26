f722be6cea388c5276fd2ce5735d723b28d79dc4
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-26 16:44:07 -0700

    Convert connect section to reusable block
    This removes the customizer fields for the reusable block and replaces
    them with a single selector for a reusable block.

--- themes/shiro/inc/classes/customizer/class-connect.php
+++ themes/shiro/inc/classes/customizer/class-connect.php
@@ -17,190 +17,40 @@ class Connect extends Base {
 	 * Add Customizer fields for header section.
 	 */
 	public function setup_fields() {
-		$panel_id = 'wmf_connect';
-		$this->customize->add_panel(
-			$panel_id, array(
+		$section_id = 'wmf_connect';
+		$this->customize->add_section(
+			$section_id, array(
 				'title'    => __( 'Connect', 'shiro-admin' ),
 				'priority' => 70,
 			)
 		);
 
-		// Headings.
-		$section_id = 'wmf_contact_headings';
-		$this->customize->add_section(
-			$section_id, array(
-				'title'    => __( 'Main Headings', 'shiro-admin' ),
-				'priority' => 70,
-				'panel'    => $panel_id,
-			)
-		);
-
-		$control_id = 'wmf_connect_pre_heading';
-		$this->customize->add_setting(
-			$control_id, array(
-				'default' => __( 'Connect', 'shiro-admin' ),
-			)
-		);
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Pre Heading', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_connect_heading';
-		$this->customize->add_setting(
-			$control_id, array(
-				'default' => __( 'Stay up-to-date on our work.', 'shiro-admin' ),
-			)
-		);
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Heading', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		// Subscribe Box.
-		$section_id = 'wmf_contact_subscribe';
-		$this->customize->add_section(
-			$section_id, array(
-				'title'    => __( 'Subscribe Box', 'shiro-admin' ),
-				'priority' => 70,
-				'panel'    => $panel_id,
-			)
-		);
-
-		$control_id = 'wmf_subscribe_heading';
-		$this->customize->add_setting(
-			$control_id, array(
-				'default' => __( 'Subscribe to our newsletter', 'shiro-admin' ),
-			)
-		);
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Heading', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_subscribe_content';
+		$control_id = 'wmf_connect_reusable_block';
 		$this->customize->add_setting( $control_id );
+		$reusable_blocks   = get_posts( [
+			'post_type'   => 'wp_block',
+			'numberposts' => 50,
+		] );
+		$selectable_blocks = [];
+		foreach ( $reusable_blocks as $block ) {
+			if ( has_block( 'shiro/contact', $block->ID )
+			     || has_block( 'shiro/mailchimp-subscribe', $block->ID ) ) {
+				$selectable_blocks[ $block->ID ] = $block->post_title;
+			}
+		}
+		// We're using `+` instead of `array_merge` because array_merge rewrites numeric IDs
+		$choices = [ 0 => 'No CTA' ] + $selectable_blocks;
 		$this->customize->add_control(
-			new Rich_Text_Control(
-				$this->customize,
-				$control_id,
-				array(
-					'label'   => __( 'Content', 'shiro-admin' ),
-					'section' => $section_id,
-				)
-			)
-		);
-
-		$control_id = 'wmf_subscribe_action';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Subscribe form action URL', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_subscribe_additional_fields';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Subscribe form additional fields', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'textarea',
-			)
-		);
-
-		$control_id = 'wmf_subscribe_placeholder';
-		$this->customize->add_setting(
-			$control_id, array(
-				'default' => __( 'Email address', 'shiro-admin' ),
-			)
-		);
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Email Placeholder', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_subscribe_button';
-		$this->customize->add_setting(
-			$control_id, array(
-				'default' => __( 'Subscribe', 'shiro-admin' ),
-			)
-		);
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Button Text', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		// Contact box.
-		$section_id = 'wmf_contact_contact';
-		$this->customize->add_section(
-			$section_id, array(
-				'title'    => __( 'Contact Box', 'shiro-admin' ),
-				'priority' => 70,
-				'panel'    => $panel_id,
-			)
-		);
-
-		$control_id = 'wmf_contact_heading';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Heading', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_contact_content';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			new Rich_Text_Control(
-				$this->customize,
-				$control_id,
-				array(
-					'label'   => __( 'Content', 'shiro-admin' ),
-					'section' => $section_id,
-				)
-			)
-		);
-
-		$control_id = 'wmf_contact_link';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'       => __( 'Link', 'shiro-admin' ),
-				'description' => __( 'This can be a URI or email address.', 'shiro-admin' ),
+			$control_id, [
+				'label'       => __( 'Connect' ),
+				'type'        => 'select',
+				'choices'     => $choices,
 				'section'     => $section_id,
-				'type'        => 'text',
-			)
-		);
-
-		$control_id = 'wmf_contact_link_text';
-		$this->customize->add_setting( $control_id );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'       => __( 'Link Text', 'shiro-admin' ),
-				'description' => __( 'If this is empty, the Link value will be used automatically.', 'shiro-admin' ),
-				'section'     => $section_id,
-				'type'        => 'text',
-			)
+				'description' => count( $selectable_blocks ) > 0
+					? __( 'Select a reusable block to be shown in the "Connect" area.', 'shiro-admin' )
+					: sprintf( __( '<strong>There are no viable reusable blocks!</strong> This reusable block must include at least one of the the Connect or the Mailchimp Subscribe blocks. Please <a href="%s">create one</a>.',
+						'shiro-admin' ), admin_url( 'edit.php?post_type=wp_block' ) ),
+			]
 		);
 	}
 
