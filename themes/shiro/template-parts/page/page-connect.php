c8945a957d8d14787a79b4fee54ff20b23804552
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-27 17:20:43 -0700

    Show reusable block connect module, if set
    The following two conditions must be true:
    - A reusable block has been set for the "Connect" option in the
      customizer
    - The page must not have a data for a customized "connect" section
    Then the reusable block will be shown.
    Otherwise, it falls back to the legacy version.
    
    The long-term intent is that the few pages using customized connect
    sections will be rebuilt using blocks in their entirety, at which point
    this logic can be greatly simplified.
    
    Based on a preprod DB dump, *possible* IDs for pages with customized
    connect sections are:
    105
    107
    109
    111
    113
    62419
    62423
    62427
    62429

--- themes/shiro/template-parts/page/page-connect.php
+++ themes/shiro/template-parts/page/page-connect.php
@@ -8,16 +8,36 @@
  */
 
 $template_args = get_post_meta( get_the_ID(), 'connect', true );
-$template_args = empty( $template_args ) || is_string( $template_args ) ? array() : $template_args;
 
-$rand_translation = wmf_get_random_translation(
-	'connect', array(
-		'source' => 'meta',
-	)
-);
+// Determine if this is using a legacy customization to the connect text
+$no_custom_connect = empty( array_filter( $template_args ) );
 
-$template_args['rand_translation_title'] = empty( $rand_translation['pre_heading'] ) ? '' : $rand_translation['pre_heading'];
+$reusable_block_id = get_theme_mod( 'wmf_connect_reusable_block' );
 
-if ( empty( $template_args['hide'] ) ) {
-	get_template_part( 'template-parts/modules/general/connect', null, $template_args );
+if ( $no_custom_connect
+     && is_numeric( $reusable_block_id )
+     && $reusable_block_id > 0
+     && get_post_type( $reusable_block_id ) === 'wp_block' ) {
+	$block = get_post( $reusable_block_id );
+	if ( is_a( $block, \WP_Post::class ) ) { ?>
+		<?php /** Since we're not in the "content" area, these blocks need
+		 * a wrapper with a set width or they look real strange. */ ?>
+		<div class="mw-980">
+			<?php echo apply_filters( 'the_content', $block->post_content ) ?>
+		</div>
+	<?php }
+} else {
+	$template_args = empty( $template_args ) || is_string( $template_args ) ? array() : $template_args;
+
+	$rand_translation = wmf_get_random_translation(
+		'connect', array(
+			'source' => 'meta',
+		)
+	);
+
+	$template_args['rand_translation_title'] = empty( $rand_translation['pre_heading'] ) ? '' : $rand_translation['pre_heading'];
+
+	if ( empty( $template_args['hide'] ) ) {
+		get_template_part( 'template-parts/modules/general/connect', null, $template_args );
+	}
 }
