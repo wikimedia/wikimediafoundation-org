b2fd5c91a7101a9793b4fa27f217fedf96899443
Ben Martinez-Bateman <ben@alwaysblank.org>
2021-05-28 10:37:53 -0700

    Use double-heading styles for related blog fallback

--- themes/shiro/template-parts/modules/related/posts.php
+++ themes/shiro/template-parts/modules/related/posts.php
@@ -22,23 +22,24 @@ $rand_translation_title = wmf_get_random_translation( 'wmf_related_posts_title' 
 ?>
 
 <div class="mw-980">
-	<?php if ( ! empty( $title ) ) : ?>
-		<h3 class="h3 color-gray uppercase">
-			<?php echo esc_html( $title ); ?>
-			<?php if ( ! empty( $rand_translation_title['content'] ) ) : ?>
-				— <span lang="<?php echo esc_attr( $rand_translation_title['lang'] ); ?>"><?php echo esc_html( $rand_translation_title['content'] ); ?></span>
-			<?php endif; ?>
-		</h3>
-	<?php endif; ?>
-
-	<?php if ( ! empty( $description ) ) : ?>
-		<h2 class="h2">
-			<?php echo esc_html( $description ); ?>
+	<?php /** This uses the structure & styles of the double heading, but can't
+	 * use render_blocks() directly because the double-heading block
+	 * structures its data very differently.
+	 */ ?>
+	<div class="double-heading">
+		<?php if ( ! empty( $title ) ) : ?>
+			<p class="double-heading__secondary is-style-h5">
+				<span><?php echo esc_html( $title ) ?></span>
+			</p>
+		<?php endif; ?>
+		<h2 class="double-heading__primary is-style-h3">
+			<?php echo esc_html( $description ) ?>
 			<?php if ( ! empty( $authorlink ) ) : ?>
-				<span class="authorlink"><a href="/news/author/<?php echo esc_attr( $authorlink ); ?>">View all</a></span>
+				<span class="authorlink"><a
+							href="/news/author/<?php echo esc_attr( $authorlink ); ?>">View all</a></span>
 			<?php endif; ?>
 		</h2>
-	<?php endif; ?>
+	</div>
 
 	<?php foreach ($template_data['posts'] as $post) {
 		echo BlogPost\render_block( [ 'post_id' => $post->ID ] );
