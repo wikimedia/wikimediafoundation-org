b08478c15fe95cd7b7c350b899c795a79669444c
Ben <ben@alwaysblank.dev>
2021-06-03 12:37:20 -0700

    Change classes on profile card module

--- themes/shiro/template-parts/modules/profiles/card.php
+++ themes/shiro/template-parts/modules/profiles/card.php
@@ -16,24 +16,24 @@ $team   = ! empty( $template_data['team'] ) ? $template_data
 ['team'] : '';
 $img_id = ! empty( $template_data['img_id'] ) ? $template_data
 ['img_id'] : '';
+$image_el = wp_get_attachment_image($img_id,  'image_4x3_small', null, [
+		'class' => 'profile__image',
+] );
 
 ?>
 
-<a class="card card-vertical card-person rounded shadow hover-img-zoom w-32p" href="<?php echo esc_url( $link ); ?>">
-	<?php if ( ! empty( $img_id ) ) : ?>
-	<div class="img-container" style="background-image:url(<?php echo esc_url(wp_get_attachment_image_url( $img_id, 'image_4x5_large' )); ?>)">
-	</div>
-	<?php endif; ?>
+<a class="profile" href="<?php echo esc_url( $link ); ?>">
+	<?php echo $image_el ?>
 
-	<div class="card-content top-into-img">
+	<div class="profile__content">
 		<?php if ( ! empty( $title ) ) : ?>
-		<h5 class="person-name">
+		<h5 class="profile__name is-style-h5">
 			<?php echo esc_html( $title ); ?>
 		</h5>
 		<?php endif; ?>
 
 		<?php if ( ! empty( $role ) ) : ?>
-			<span class="person-title p color-gray">
+			<span class="profile__title">
 				<?php
 				if ( $role && $team ) {
 					esc_html_e(
