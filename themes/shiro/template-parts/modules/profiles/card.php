2f35ac291046cb7859552944e9f1b25e4354e56a
Ben <ben@alwaysblank.dev>
2021-06-11 16:53:46 -0700

    Improve card accessibility

--- themes/shiro/template-parts/modules/profiles/card.php
+++ themes/shiro/template-parts/modules/profiles/card.php
@@ -7,29 +7,29 @@
 
 $template_data = $args;
 
-$link   = ! empty( $template_data['link'] ) ? $template_data['link'] : '';
-$title  = ! empty( $template_data['title'] ) ? $template_data
+$link     = ! empty( $template_data['link'] ) ? $template_data['link'] : '';
+$title    = ! empty( $template_data['title'] ) ? $template_data
 ['title'] : '';
-$role   = ! empty( $template_data['role'] ) ? $template_data
+$role     = ! empty( $template_data['role'] ) ? $template_data
 ['role'] : '';
-$team   = ! empty( $template_data['team'] ) ? $template_data
+$team     = ! empty( $template_data['team'] ) ? $template_data
 ['team'] : '';
-$img_id = ! empty( $template_data['img_id'] ) ? $template_data
+$img_id   = ! empty( $template_data['img_id'] ) ? $template_data
 ['img_id'] : '';
-$image_el = wp_get_attachment_image($img_id,  'image_4x3_small', null, [
-		'class' => 'profile__image',
+$image_el = wp_get_attachment_image( $img_id, 'image_4x3_small', null, [
+	'class' => 'profile__image',
 ] );
 
 ?>
 
-<a class="profile" href="<?php echo esc_url( $link ); ?>">
+<div class="profile">
 	<?php echo $image_el ?>
 
 	<div class="profile__content">
 		<?php if ( ! empty( $title ) ) : ?>
-		<h5 class="profile__name is-style-h5">
-			<?php echo esc_html( $title ); ?>
-		</h5>
+			<h5 class="profile__name is-style-h5">
+				<a class="profile__link" href="<?php echo esc_url( $link ); ?>"><?php echo esc_html( $title ); ?></a>
+			</h5>
 		<?php endif; ?>
 
 		<?php if ( ! empty( $role ) ) : ?>
@@ -38,8 +38,8 @@ $image_el = wp_get_attachment_image($img_id,  'image_4x3_small', null, [
 				if ( $role && $team ) {
 					esc_html_e(
 					/* translators: 1. role 2. team */
-					sprintf( '%1$s, %2$s', $role, $team ),
-					'shiro'
+						sprintf( '%1$s, %2$s', $role, $team ),
+						'shiro'
 					);
 				} else {
 					echo esc_html( $role );
@@ -48,5 +48,4 @@ $image_el = wp_get_attachment_image($img_id,  'image_4x3_small', null, [
 			</span>
 		<?php endif; ?>
 	</div>
-
-</a>
+</div>
