823d8be9f3e9cbfe2b064c3e0c5f3d871bee18cb
Ben <ben@alwaysblank.dev>
2021-06-15 10:59:22 -0700

    Update react-select to remove warning

--- plugins/hm-gutenberg-tools/.config/webpack.config.prod.js
+++ plugins/hm-gutenberg-tools/.config/webpack.config.prod.js
@@ -2,8 +2,10 @@
  * .config/webpack.config.prod.js :
  * This file defines the production build configuration
  */
-const { helpers, externals, presets } = require( '@humanmade/webpack-helpers' );
+const { helpers, externals, presets, loaders } = require( '@humanmade/webpack-helpers' );
 const { filePath } = helpers;
+
+loaders.file.defaults.exclude = [/\.(js|html|json)$/, /node_modules/];
 
 module.exports = presets.production( {
 	name: 'editor',
