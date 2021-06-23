ec2a88c4e8155d27866ebda5f414b888b229be75
Ben <ben@alwaysblank.dev>
2021-06-23 14:18:36 -0700

    Escape action for searchform.php
    re: https://github.com/wpcomvip/wikimediafoundation-org/pull/608#discussion_r656422979

--- themes/shiro/searchform.php
+++ themes/shiro/searchform.php
@@ -8,7 +8,7 @@
 $wmf_search_button      = get_theme_mod( 'wmf_search_button_copy', __( 'Search', 'shiro-admin' ) );
 $wmf_search_placeholder = get_theme_mod( 'wmf_search_placeholder_copy', __( 'Search', 'shiro-admin' ) );
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="search-form__label">
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
 		<input type="search" class="search-form__field"
