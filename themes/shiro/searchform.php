ee0465c4ab6512bce308c353d8dd9ff6320660db
Ben <ben@alwaysblank.dev>
2021-06-23 14:20:29 -0700

    Escape searchform label
    re: https://github.com/wpcomvip/wikimediafoundation-org/pull/608#discussion_r656422981

--- themes/shiro/searchform.php
+++ themes/shiro/searchform.php
@@ -10,7 +10,7 @@ $wmf_search_placeholder = get_theme_mod( 'wmf_search_placeholder_copy', __( 'Sea
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="search-form__label">
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label' ) ?></span>
 		<input type="search" class="search-form__field"
 			   placeholder="<?php echo esc_attr_x( $wmf_search_placeholder, 'placeholder' ) ?>"
 			   value="<?php echo get_search_query() ?>" name="s"
