273a121614ff308b83cfcc954bc16f9cad9e9477
Joeleen Kennedy <joeleen@humanmade.com>
2021-07-07 18:53:28 -0400

    Restructure role html, styles

--- themes/shiro/template-parts/profiles/role-item.php
+++ themes/shiro/template-parts/profiles/role-item.php
@@ -20,11 +20,13 @@ if ( ! $post ) {
 
 ?>
 
-<a class="staff-list-item mar-bottom_lg" href="<?php echo esc_url( get_the_permalink( $post_id ) ); ?>">
-	<div class="staff-photo" style="background-image: url('<?php echo esc_url( get_the_post_thumbnail_url( $post_id, 'profile_thumb' ) ); ?>')"></div>
+<li class="role__staff-list__item">
+	<div class="role__staff-list__item__photo" style="background-image: url('<?php echo esc_url( get_the_post_thumbnail_url( $post_id, 'profile_thumb' ) ); ?>')"></div>
 	<h4>
-		<?php echo esc_html( get_the_title( $post_id ) ); ?>
+		<a href="<?php echo esc_url( get_the_permalink( $post_id ) ); ?>">
+			<?php echo esc_html( get_the_title( $post_id ) ); ?>
+		</a>
 	</h4>
 
-	<span><?php echo esc_html( get_post_meta( $post_id, 'profile_role', true ) ); ?></span>
-</a>
+	<p><?php echo esc_html( get_post_meta( $post_id, 'profile_role', true ) ); ?></p>
+</li>
--- themes/shiro/template-parts/profiles/role-list.php
+++ themes/shiro/template-parts/profiles/role-list.php
@@ -21,59 +21,57 @@ foreach ( $post_list as $term_id => $term_data ) :
 	$name        = ( ! $show_heading && ( is_wp_error( $term ) || empty( $term->parent ) ) ) ? '' : $name;
 	?>
 
-<div class="static-list-item mod-margin-bottom_xs wysiwyg">
+<section class="role__section wysiwyg">
 
 	<?php if ( ! empty( $name ) ) : ?>
-	<h2 class="static-list-heading" id="section-<?php echo absint( $term_id ); ?>">
+	<h2 class="role__heading" id="section-<?php echo absint( $term_id ); ?>">
 		<?php echo esc_html( $name ); ?>
 	</h2>
 	<?php endif; ?>
 
-	<div class="static-list-contents">
+	<?php
+	if ( ! empty( $description ) ) {
+		echo wp_kses_post( $description );
+	}
+	?>
+
+	<?php if ( ! empty( $term_data ) ) : ?>
+	<ul class="role__staff-list">
 		<?php
-		if ( ! empty( $description ) ) {
-			echo wp_kses_post( $description );
+		foreach ( $term_data['posts'] as $post_id ) {
+			get_template_part(
+				'template-parts/profiles/role',
+				'item',
+				array(
+					'id' => $post_id,
+				)
+			);
 		}
 		?>
+	</ul>
+	<?php endif; ?>
 
-		<?php if ( ! empty( $term_data ) ) : ?>
-		<div class="mod-margin-bottom_xs staff-list">
-			<?php
-			foreach ( $term_data['posts'] as $post_id ) {
-				get_template_part(
-					'template-parts/profiles/role',
-					'item',
-					array(
-						'id' => $post_id,
-					)
-				);
-			}
-			?>
+	<?php
+	if ( ! empty( $button['link_to_archive'] ) ) :
+		$link_text = ! empty( $button['text'] )
+			? $button['text']
+			: __(
+				sprintf(
+					/* translators: The name of the current taxonomy. */
+					'View full %s team',
+					$name
+				),
+				'shiro'
+			);
+		$link_url  = ! empty( $button['link'] ) ? $button['link'] : get_term_link( $term_id, 'role' );
+	?>
+		<div class="role__read-more">
+			<a href="<?php echo esc_url( $link_url ); ?>" class="arrow-link">
+				<?php echo esc_html( $link_text ); ?>
+			</a>
 		</div>
-		<?php endif; ?>
+	<?php endif; ?>
 
-		<?php
-		if ( ! empty( $button['link_to_archive'] ) ) :
-			$link_text = ! empty( $button['text'] )
-				? $button['text']
-				: __(
-					sprintf(
-						/* translators: The name of the current taxonomy. */
-						'View full %s team',
-						$name
-					),
-					'shiro'
-				);
-			$link_url  = ! empty( $button['link'] ) ? $button['link'] : get_term_link( $term_id, 'role' );
-		?>
-			<div class="mod-margin-bottom_sm">
-				<a href="<?php echo esc_url( $link_url ); ?>" class="arrow-link">
-					<?php echo esc_html( $link_text ); ?>
-				</a>
-			</div>
-		<?php endif; ?>
-	</div>
-
-</div>
+</section>
 	<?php
 endforeach;
