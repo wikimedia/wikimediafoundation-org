abb5e38a7c560558cf55c6e90d5c761d239a05f3
Joeleen Kennedy <joeleen@humanmade.com>
2021-07-07 14:03:18 -0400

    Remove 3rd level role nesting

--- themes/shiro/template-parts/profiles/role-list.php
+++ themes/shiro/template-parts/profiles/role-list.php
@@ -55,54 +55,6 @@ foreach ( $post_list as $term_id => $term_data ) :
 			?>
 		</div>
 		<?php endif; ?>
-
-		<?php if ( ! empty( $term_data['children'] ) ) : ?>
-		<div>
-			<?php
-			foreach ( $term_data['children'] as $child_term_id => $child_term_data ) :
-				$name        = ! empty( $child_term_data['name'] ) ? $child_term_data['name'] : '';
-				$description = term_description( $child_term_id, 'role' );
-				$button      = get_term_meta( $child_term_id, 'role_button', true );
-				$link        = ! empty( $button['link'] ) ? $button['link'] : '';
-				?>
-
-				<?php if ( ! empty( $name ) ) : ?>
-				<h3 class="mar-bottom">
-					<?php if ( ! empty( $link ) ) : ?>
-					<a href="<?php echo esc_url( $link ); ?>">
-					<?php endif; ?>
-
-						<?php echo esc_html( $name ); ?>
-
-					<?php if ( ! empty( $link ) ) : ?>
-						<?php wmf_show_icon( 'open', 'material' ); ?>
-					</a>
-					<?php endif; ?>
-				</h3>
-				<?php endif; ?>
-
-				<?php if ( ! empty( $description ) ) : ?>
-				<p class="mar-bottom_lg"><?php echo wp_kses_post( $description ); ?></p>
-				<?php endif; ?>
-
-				<div class="mod-margin-bottom_xs staff-list">
-					<?php
-
-					foreach ( $child_term_data['posts'] as $post_id ) :
-						get_template_part(
-							'template-parts/profiles/role',
-							'item',
-							array(
-								'id' => $post_id,
-							)
-						);
-					endforeach;
-					?>
-				</div>
-
-			<?php endforeach; ?>
-		</div>
-		<?php endif; ?>
 	</div>
 
 </div>
