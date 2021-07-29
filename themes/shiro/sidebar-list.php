4c6eecddd44c810443a4f03db1a29ccbf2fb113e
Joeleen Kennedy <joeleen@humanmade.com>
2021-07-29 16:31:05 -0400

    Make sure template_args is array before using it.
    Co-authored-by: Ben <ben@alwaysblank.dev>

--- themes/shiro/sidebar-list.php
+++ themes/shiro/sidebar-list.php
@@ -9,7 +9,7 @@
 
 // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 $nested        = $args['nested'] ?? false;
-$template_args = $args['template_args'] ?? get_post_meta( get_the_ID(), 'list', true );
+$template_args = is_array( $args['template_args'] ) ? $args['template_args'] : get_post_meta( get_the_ID(), 'list', true );
 // phpcs:enable
 
 if ( empty( $template_args ) ) {
