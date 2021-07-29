0e860eb7ef3ebbcbc67d9eb8f4226dc33c3ac9ff
Joeleen Kennedy <joeleen@humanmade.com>
2021-07-29 15:59:00 -0400

    Use a real image, fix weird responsive breakpoint

--- themes/shiro/template-parts/profiles/role-item.php
+++ themes/shiro/template-parts/profiles/role-item.php
@@ -11,8 +11,13 @@ if ( empty( $post_data['id'] ) ) {
 	return;
 }
 
-$post_id = $post_data['id'];
-$post    = get_post( $post_id );
+$post_id  = $post_data['id'];
+$post     = get_post( $post_id );
+$post_img = get_the_post_thumbnail(
+	$post_id,
+	'profile_thumb',
+	[ 'class' => 'role__staff-list__item__photo' ]
+);
 
 if ( ! $post ) {
 	return;
@@ -20,8 +25,16 @@ if ( ! $post ) {
 
 ?>
 
+
 <li class="role__staff-list__item">
-	<div class="role__staff-list__item__photo" style="background-image: url('<?php echo esc_url( get_the_post_thumbnail_url( $post_id, 'profile_thumb' ) ); ?>')"></div>
+	<?php
+	if ( $post_img ) :
+		echo wp_kses_post( $post_img );
+	else :
+	?>
+		<span class="role__staff-list__item__photo"></span>
+	<?php endif; ?>
+
 	<h4>
 		<a href="<?php echo esc_url( get_the_permalink( $post_id ) ); ?>">
 			<?php echo esc_html( get_the_title( $post_id ) ); ?>
