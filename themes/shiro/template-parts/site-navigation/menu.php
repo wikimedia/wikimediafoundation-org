964d098b473ca1b85acd5afab9012526d9b504b9
Joeleen Kennedy <joeleen@humanmade.com>
2021-11-10 11:24:45 -0500

    206: start setting up subnav

--- themes/shiro/template-parts/site-navigation/menu.php
+++ themes/shiro/template-parts/site-navigation/menu.php
@@ -12,13 +12,8 @@ if ( has_nav_menu( 'header' ) ) {
 			'theme_location' => 'header',
 			'menu_class'     => 'primary-nav__items',
 			'container'      => '',
-			// Remove this when implementing multi-level navigation.
-			// For now it's here because the existing menu implementation
-			// doesn't support more than one level and looks very odd if
-			// you try.
-			'depth'          => 1,
+			'depth'          => 2,
 		)
 	);
 }
 ?>
-
