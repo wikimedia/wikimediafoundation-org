c7bfe4a21abe59717a69b7e2f5500d909f846e03
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-02-08 06:33:01 -0800

    Add the page slug as a body class

--- themes/shiro/functions.php
+++ themes/shiro/functions.php
@@ -465,3 +465,20 @@ function link_reusable_blocks_url() {
 }
 
 add_action( 'admin_menu', 'link_reusable_blocks_url' );
+
+/**
+ * Add page slug as body class.
+ *
+ * @param array $classes An array of body class names.
+ * @return array
+ */
+function shiro_add_slug_body_class( $classes ) {
+	global $post;
+
+	if ( isset( $post ) ) {
+		$classes[] = $post->post_type . '-' . $post->post_name;
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'shiro_add_slug_body_class' );
