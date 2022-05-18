327007210168e16cdd24a79f36fd10e1d921eaae
Ben <ben@alwaysblank.dev>
2022-05-18 14:53:30 -0700

    Allow the title to be returned for guest authors in certain cases

--- themes/shiro/inc/editor/profile-fields.php
+++ themes/shiro/inc/editor/profile-fields.php
@@ -77,4 +77,39 @@ function wmf_setup_profile_fields_for_editor() {
 	] );
 }
 
+/**
+ * If the query is a search for guest-authors, add title support to guest authors.
+ *
+ * This is necessary because otherwise REST API doesn't return a title (because
+ * guest-author removes title support from itself).
+ *
+ * @param \WP_Query $query
+ *
+ * @return void
+ */
+function wmf_enable_guest_author_titles_when_searching( WP_Query $query ) {
+	if ( $query->is_search()
+	     && in_array( 'guest-author', $query->get( 'post_type', [] ), true ) ) {
+		add_post_type_support( 'guest-author', [ 'title' ] );
+	}
+}
+
+/**
+ * Because Guest Authors disable `title`, we need to add that value to what REST queries return.
+ *
+ * @param \WP_REST_Response $response
+ * @param \WP_Post $post
+ *
+ * @return \WP_REST_Response
+ */
+function wmf_add_author_name_to_guest_author_rest_response( WP_REST_Response $response, WP_Post $post ) {
+	$data = $response->get_data();
+	$data['author_name'] = $post->post_title;
+	$response->set_data( $data );
+
+	return $response;
+}
+
+add_action( 'rest_prepare_guest-author', 'wmf_add_author_name_to_guest_author_rest_response', 10, 2 );
+add_action( 'pre_get_posts', 'wmf_enable_guest_author_titles_when_searching', 100 );
 add_action( 'init', 'wmf_setup_profile_fields_for_editor' );
