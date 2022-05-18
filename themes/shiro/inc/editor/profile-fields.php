f0a4aafdc78ab5a282326d53a59c7bac2c563830
Ben <ben@alwaysblank.dev>
2022-05-17 17:02:48 -0700

    Add sanitization for fields

--- themes/shiro/inc/editor/profile-fields.php
+++ themes/shiro/inc/editor/profile-fields.php
@@ -1,7 +1,7 @@
 <?php
 
 function wmf_setup_profile_fields_for_editor() {
-	$register = function( $key, $args ) {
+	$register = function ( $key, $args ) {
 		register_meta(
 			'post',
 			$key,
@@ -16,10 +16,32 @@ function wmf_setup_profile_fields_for_editor() {
 		);
 	};
 
-	$register( 'last_name', [ 'type' => 'string', ] );
-	$register( 'profile_role', [ 'type' => 'string', ] );
-	$register( 'profile_featured', [ 'type' => 'boolean', ] );
-	$register( 'connected_user', [ 'type' => 'integer' ] );
+	$register( 'last_name', [
+		'type' => 'string',
+		'sanitize_callback' => 'sanitize_text_field',
+		'default' => '',
+	] );
+	$register( 'profile_role', [
+		'type' => 'string',
+		'sanitize_callback' => 'sanitize_text_field',
+		'default' => '',
+	] );
+	$register( 'profile_featured', [
+		'type' => 'boolean',
+		'sanitize_callback' => 'rest_sanitize_boolean',
+		'default' => false,
+	] );
+	$register( 'connected_user', [
+		'type' => 'integer',
+		'sanitize_callback' => function ( $value ) {
+			if ( is_numeric( $value ) ) {
+				return intval( $value );
+			}
+
+			return 0;
+		},
+		'default' => 0,
+	] );
 	$register( 'contact_links', [
 		'type' => 'array',
 		'show_in_rest' => [
@@ -33,10 +55,25 @@ function wmf_setup_profile_fields_for_editor() {
 						'link' => [
 							'type' => 'string',
 						],
-					]
-				]
-			]
-		]
+					],
+				],
+			],
+		],
+		'sanitize_callback' => function ( $value ) {
+			if ( ! is_array( $value ) ) {
+				return [];
+			}
+
+			return array_filter( $value, function ( $row ) {
+				// Both items must be set to a non-empty value.
+				if ( isset( $row['title'] ) && isset( $row['link'] ) &&
+				     ! empty( $row['title'] ) && ! empty( $row['link'] ) ) {
+					return true;
+				}
+
+				return false;
+			} );
+		},
 	] );
 }
 
