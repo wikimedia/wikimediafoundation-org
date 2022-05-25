d9459285f5de8f33e782c589da7a20f99ea626c6
Ben <ben@alwaysblank.dev>
2022-05-25 09:49:13 -0700

    Use short array syntax, again

--- themes/shiro/inc/fields/links.php
+++ themes/shiro/inc/fields/links.php
@@ -14,37 +14,37 @@ function wmf_links_fields() {
 	}
 
 	$links = new Fieldmanager_Group(
-		array(
+		[
 			'name'     => 'off_site_links',
-			'children' => array(
+			'children' => [
 				'heading' => new Fieldmanager_Textfield( __( 'Section Heading', 'shiro-admin' ) ),
 				'links'   => new Fieldmanager_Group(
-					array(
+					[
 						'add_more_label' => __( 'Add Link', 'shiro-admin' ),
 						'sortable'       => true,
 						'limit'          => 0,
-						'children'       => array(
+						'children'       => [
 							'heading' => new Fieldmanager_Textfield( __( 'Heading', 'shiro-admin' ) ),
 							'uri'     => new Fieldmanager_Link( __( 'URI', 'shiro-admin' ) ),
 							'content' => new Fieldmanager_RichTextArea(
-								array(
+								[
 									'label'           => __( 'Content', 'shiro-admin' ),
-									'buttons_1'       => array( 'bold', 'italic', 'strikethrough', 'underline' ),
-									'buttons_2'       => array(),
-									'editor_settings' => array(
+									'buttons_1'       => [ 'bold', 'italic', 'strikethrough', 'underline' ],
+									'buttons_2'       => [],
+									'editor_settings' => [
 										'quicktags'     => false,
 										'media_buttons' => false,
-									),
-								)
+									],
+								]
 							),
-						),
-					)
+						],
+					]
 				),
 
-			),
-		)
+			],
+		]
 	);
-	$links->add_meta_box( __( 'Off Site Links', 'shiro-admin' ), array( 'page', 'post' ) );
+	$links->add_meta_box( __( 'Off Site Links', 'shiro-admin' ), [ 'page', 'post' ] );
 }
 add_action( 'fm_post_post', 'wmf_links_fields' );
 add_action( 'fm_post_page', 'wmf_links_fields' );
