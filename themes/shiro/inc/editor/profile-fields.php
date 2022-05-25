d81633a6d2ef19d1f71ce3e387278e14de1bacbd
Ben <ben@alwaysblank.dev>
2022-05-25 09:30:46 -0700

    Clean up profile-fields registration flow

--- themes/shiro/inc/editor/profile-fields.php
+++ themes/shiro/inc/editor/profile-fields.php
@@ -1,80 +1,111 @@
 <?php
 
+/**
+ * Register a single profile field.
+ *
+ * @param array $registry Array containing key and arguments.
+ *
+ * @return void
+ */
+function wmf_register_profile_field( array $registry ) {
+	[ 'key' => $key, 'args' => $args ] = $registry;
+	$default_register_meta_settings = [
+		'single' => true,
+		'show_in_rest' => true,
+		'object_subtype' => 'profile',
+	];
+	$compiled_args = array_merge( $default_register_meta_settings, $args );
+	register_post_meta(
+		'profile',
+		$key,
+		apply_filters( 'profile_field_args', $compiled_args )
+	);
+}
+
+/**
+ * Set up all profile fields.
+ *
+ * @return void
+ */
 function wmf_setup_profile_fields_for_editor() {
-	$register = function ( $key, $args ) {
-		register_meta(
-			'post',
-			$key,
-			array_merge(
-				[
-					'single' => true,
-					'show_in_rest' => true,
-					'object_subtype' => 'profile',
-				],
-				$args
-			)
-		);
-	};
+	$profile_fields = [
+		[
+			'key' => 'last_name',
+			'args' => [
+				'type' => 'string',
+				'sanitize_callback' => 'sanitize_text_field',
+				'default' => '',
+			],
+		],
+		[
+			'key' => 'profile_role',
+			'args' => [
+				'type' => 'string',
+				'sanitize_callback' => 'sanitize_text_field',
+				'default' => '',
+			],
+		],
+		[
+			'key' => 'profile_featured',
+			'args' => [
+				'type' => 'boolean',
+				'sanitize_callback' => 'rest_sanitize_boolean',
+				'default' => false,
+			],
+		],
+		[
+			'key' => 'connected_user',
+			'args' => [
+				'type' => 'integer',
+				'sanitize_callback' => function ( $value ) {
+					if ( is_numeric( $value ) ) {
+						return intval( $value );
+					}
 
-	$register( 'last_name', [
-		'type' => 'string',
-		'sanitize_callback' => 'sanitize_text_field',
-		'default' => '',
-	] );
-	$register( 'profile_role', [
-		'type' => 'string',
-		'sanitize_callback' => 'sanitize_text_field',
-		'default' => '',
-	] );
-	$register( 'profile_featured', [
-		'type' => 'boolean',
-		'sanitize_callback' => 'rest_sanitize_boolean',
-		'default' => false,
-	] );
-	$register( 'connected_user', [
-		'type' => 'integer',
-		'sanitize_callback' => function ( $value ) {
-			if ( is_numeric( $value ) ) {
-				return intval( $value );
-			}
-
-			return 0;
-		},
-		'default' => 0,
-	] );
-	$register( 'contact_links', [
-		'type' => 'array',
-		'show_in_rest' => [
-			'schema' => [
-				'items' => [
-					'type' => 'object',
-					'properties' => [
-						'title' => [
-							'type' => 'string',
-						],
-						'link' => [
-							'type' => 'string',
+					return 0;
+				},
+				'default' => 0,
+			],
+		],
+		[
+			'key' => 'contact_links',
+			'args' => [
+				'type' => 'array',
+				'show_in_rest' => [
+					'schema' => [
+						'items' => [
+							'type' => 'object',
+							'properties' => [
+								'title' => [
+									'type' => 'string',
+								],
+								'link' => [
+									'type' => 'string',
+								],
+							],
 						],
 					],
 				],
+				'sanitize_callback' => function ( $value ) {
+					if ( ! is_array( $value ) ) {
+						return [];
+					}
+
+					return array_filter( $value, function ( $row ) {
+						// Both items must be set to a non-empty value.
+						if ( isset( $row['title'] ) && isset( $row['link'] ) &&
+						     ! empty( $row['title'] ) && ! empty( $row['link'] ) ) {
+							return true;
+						}
+
+						return false;
+					} );
+				},
 			],
 		],
-		'sanitize_callback' => function ( $value ) {
-			if ( ! is_array( $value ) ) {
-				return [];
-			}
+	];
 
-			return array_filter( $value, function ( $row ) {
-				// Both items must be set to a non-empty value.
-				if ( isset( $row['title'] ) && isset( $row['link'] ) &&
-				     ! empty( $row['title'] ) && ! empty( $row['link'] ) ) {
-					return true;
-				}
-
-				return false;
-			} );
-		},
-	] );
+	array_map( 'wmf_register_profile_field', $profile_fields );
 }
 
 /**
