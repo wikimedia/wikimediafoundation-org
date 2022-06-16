21fbddd7d3413b5b014eed9fcdf2499ce771600e
Gregory Varnum <674625+varnent@users.noreply.github.com>
2022-06-16 05:51:18 +0000

    Build changes from 4f05ab2b8da3de7d2ec5af06fbef0f170b8fc73e
    Merge pull request humanmade/Wikimedia#460 from humanmade/revert-345-implement-language-taxonomy
    
    Revert 345 implement language taxonomy

--- themes/shiro/functions.php
+++ themes/shiro/functions.php
@@ -317,7 +317,6 @@ require get_template_directory() . '/inc/fields.php';
 /**
  * Custom Taxonomies.
  */
-require get_template_directory() . '/inc/taxonomy-content-language.php';
 require get_template_directory() . '/inc/taxonomies.php';
 
 /**
