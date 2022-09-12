7fa9236bde4e366677c3b01ccbd8703a8c5444e8
Pamela Ribeiro <contato@pamelanascimento.com>
2022-09-12 16:38:15 -0300

    Wikipedia Preview update

--- plugins/wikipedia-preview/scripts/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir libs
-cp node_modules/wikipedia-preview/dist/wikipedia-preview.production.js libs/
-cp node_modules/wikipedia-preview/dist/wikipedia-preview-link.css libs/
-npm run build
