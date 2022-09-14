b8b9ecb12cc256e3641cefee266aff4591cd25d6
K. Adam White <kadamwhite@users.noreply.github.com>
2022-09-14 14:48:51 -0500

    Restore more PHP changes from #411

--- themes/shiro/inc/editor/blocks/blog-list.php
+++ themes/shiro/inc/editor/blocks/blog-list.php
@@ -36,6 +36,12 @@ function register_block() {
 						'type' => 'object',
 					],
 				],
+				'excludedCategories' => [
+					'type' => 'array',
+					'items' => [
+						'type' => 'object',
+					],
+				],
 				'order' => [
 					'type' => 'string',
 					'default' => 'desc',
@@ -59,7 +65,7 @@ function register_block() {
  *
  * @return string HTML markup.
  */
-function render_block( $attributes ) {
+function render_block( $attributes ) : string {
 
 	$args = [
 		'posts_per_page'   => $attributes['postsToShow'],
@@ -89,6 +95,33 @@ function render_block( $attributes ) {
 		$args['author'] = $attributes['selectedAuthor'];
 	}
 
+	if ( wmf_get_current_content_language_term() !== null ) {
+		/*
+		 * This allows for a special case where translated posts will *not* be
+		 * filtered out: If the block has `Translations` selected as one of the
+		 * categories to show, then we *don't* filter out non-main languages.
+		 * To do so would almost certainly result in no posts being returned.
+		 */
+		$in_translated = array_reduce( $args['category__in'], function( $collected, $cat_id ) {
+			if ( $collected === true ) {
+				return true;
+			}
+			$term = get_term( $cat_id, 'category' );
+
+			return $term->slug === 'translations';
+		}, false );
+
+		if ( ! $in_translated ) {
+			$args['tax_query'] = [
+				[
+					'taxonomy' => 'content-language',
+					'field' => 'term_id',
+					'terms' => [ wmf_get_current_content_language_term()->term_id ],
+				]
+			];
+		}
+	}
+
 	$recent_posts = get_posts( $args );
 
 	if ( count( $recent_posts ) > 0 ) {
@@ -100,4 +133,6 @@ function render_block( $attributes ) {
 
 		return $output;
 	}
+
+	return '';
 }
