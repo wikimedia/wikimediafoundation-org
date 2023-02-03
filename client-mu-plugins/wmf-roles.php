615c0736a5bbf968d9a1fd8b3b632500c6c243be
Pamela Ribeiro <contato@pamelanascimento.com>
2023-02-03 15:16:32 -0300

    Update wmf-roles.php

--- client-mu-plugins/wmf-roles.php
+++ client-mu-plugins/wmf-roles.php
@@ -3,3 +3,21 @@
  * Add new role: WMF Editor.
  */
 wpcom_vip_add_role( 'wmf-editor', 'WMF Editor', get_role( 'author' )->capabilities );
+
+add_action(
+	'admin_init',
+	function() {
+		$ver = 1; // Incrementally update each time this code is changed.
+
+		// Check if this has been run already.
+		if ( $ver <= get_option( 'myplugin_roles_version' ) ) {
+			return;
+		}
+
+		// Add a Reviewer role.
+		wpcom_vip_add_role( 'wmf-editor', 'WMF Editor', get_role( 'author' )->capabilities );
+
+		// Update the version to prevent this running again.
+		update_option( 'myplugin_roles_version', $ver );
+	}
+);
