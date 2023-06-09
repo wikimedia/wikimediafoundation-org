6e81a85111693e9ca1b11afab7e9200755ec5f7d
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2023-06-09 13:56:12 -0400

    Disable REST API for users without admin privileges

---
+++ client-mu-plugins/wmf-rest-api/wmf-rest-api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Wikimedia Foundation REST API
+ * Version: 1.0.0
+ * Plugin URI: http://disneyparksnews.com/
+ * Description: Modifies the WordPress REST API functionality.
+ * Author: Human Made
+ * Author URI: https://humanmade.com
+ * Network: true
+ *
+ * @package wmf-rest-api
+ */
+
+namespace WMF\RESTAPI;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+bootstrap();
